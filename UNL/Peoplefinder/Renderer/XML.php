6a2e07b7417c400346e4235c1ae5d229db97e3cf
Brett Bieber <brett.bieber@gmail.com>
2009-01-30 21:02:10 +0000

    Add XML output.

--- UNL/Peoplefinder/Renderer/XML.php
+++ UNL/Peoplefinder/Renderer/XML.php
@@ -1,13 +1,43 @@
 <?php
-class Peoplefinder_Renderer_XML
+class UNL_Peoplefinder_Renderer_XML
 {
 
     protected $trustedIP = false;
     protected $displayStudentTelephone = false;
     
-    public function renderRecord(Peoplefinder_Record $r)
+    protected $sent_headers = false;
+    
+    function sendHeaders()
     {
-        
+        if ($this->sent_headers) {
+            return;
+        }
+        header('Content-type: text/xml');
+        echo '<?xml version="1.0" encoding="utf-8"?>
+<unl xmlns="http://wdn.unl.edu/xml">'.PHP_EOL;
+        $this->sent_headers = true;
+    }
+    
+    public function renderRecord(UNL_Peoplefinder_Record $r)
+    {
+        $this->sendHeaders();
+        echo '<person>';
+        foreach (get_object_vars($r) as $key=>$val) {
+            echo "<$key>{$val}</$key>\n";
+        }
+        echo '</person>'.PHP_EOL;
+    }
+    
+    function __destruct()
+    {
+        if ($this->sent_headers) {
+            $this->sendFooter();
+        }
+    }
+    
+    function sendFooter()
+    {
+        echo '</unl>';
     }
 }
 ?>
