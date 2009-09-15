bb2b046d97b3c01885bf0a765d3d071eae899881
Brett Bieber <brett.bieber@gmail.com>
2009-09-15 01:04:38 +0000

    Work on peoplefinder department class

---
+++ UNL/Peoplefinder/Department.php
@@ -0,0 +1,55 @@
+<?php
+require_once 'UNL/LDAP.php';
+
+class UNL_Peoplefinder_Department implements Countable, Iterator
+{
+    public $name;
+    
+    protected $_results;
+    
+    function __construct($name)
+    {
+        $this->name = $name;
+        $options = array(
+            'bind_dn'       => UNL_Peoplefinder::$bindDN,
+            'bind_password' => UNL_Peoplefinder::$bindPW,
+            );
+        
+        $ldap = UNL_LDAP::getConnection($options);
+        $name = str_replace(array('(',')','*','\'','"'), '', $name);
+        $this->_results = $ldap->search(UNL_Peoplefinder::$baseDN,
+                                        '(unlHRPrimaryDepartment='.$name.')');
+        $this->results->sort('cn');
+    }
+    
+    function count()
+    {
+        return count($this->_results);
+    }
+    
+    function rewind()
+    {
+        $this->_results->rewind();
+    }
+    
+    function current()
+    {
+        return $this->_results->current();
+    }
+    
+    function key()
+    {
+        return $this->_results->key();
+    }
+    
+    function next()
+    {
+        $this->_results->next();
+    }
+    
+    function valid()
+    {
+        return $this->_results->valid();
+    }
+}
+?>
