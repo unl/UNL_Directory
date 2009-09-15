2c59a3798569393752456c5f964b4ef688a37d1e
Brett Bieber <brett.bieber@gmail.com>
2009-09-15 03:00:40 +0000

    Increase size of search box

--- departments/index.php
+++ departments/index.php
@@ -31,8 +31,8 @@ $page->maincontentarea = <<<FORM
 <p>Search for UNL departments:</p>
 <form method="get" action="?">
     <div>
-    <label for="q">Search Departments:&nbsp;</label> 
-    <input style="width:18ex;" type="text" value="$q" id="q" name="q" /> 
+    <label for="q">Search:&nbsp;</label> 
+    <input style="width:18em;" type="text" value="$q" id="q" name="q" /> 
     <input style="margin-bottom:-7px;" name="submitbutton" type="image" src="/ucomm/templatedependents/templatecss/images/go.gif" value="Submit" id="submitbutton" />
     </div> 
 </form>
