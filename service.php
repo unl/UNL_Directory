f3b711efd590cbd6488229cbc32ccbfcf983abb1
Brett Bieber <brett.bieber@gmail.com>
2009-11-05 17:21:49 +0000

    Convert tabs to spaces.

--- service.php
+++ service.php
@@ -44,40 +44,40 @@ require_once 'UNL/Peoplefinder/Renderer/'.$renderer_class.'.php';
 $renderer_class = 'UNL_Peoplefinder_Renderer_'.$renderer_class;
 $renderer = new $renderer_class($renderer_options);
 if (isset($_GET['q']) && !empty($_GET['q'])) {
-	// Basic query, build filter and display results
-	if (strlen($_GET['q']) > 3) {
-		if (is_numeric(str_replace('-','',str_replace('(','',str_replace(')','',$_GET['q']))))) {
-			$records = $peepObj->getPhoneMatches($_GET['q']);
-			$renderer->renderSearchResults($records);
-		} else {
-			$records = $peepObj->getExactMatches($_GET['q']);
-			if (count($records) > 0) {
-			    if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
-				    echo "<div class='cMatch'>Exact matches:</div>\n";
-			    }
-				$renderer->renderSearchResults($records);
-			} else {
-			    if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
-				    echo 'No exact matches found.';
-			    }
-			}
-			if (count($records) < UNL_Peoplefinder::$displayResultLimit) {
-				// More room to display LIKE results
-				UNL_Peoplefinder::$displayResultLimit = UNL_Peoplefinder::$displayResultLimit-$peepObj->lastResultCount;
-				$records = $peepObj->getLikeMatches($_GET['q'], $records);
-				if (count($records) > 0) {
-				    if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
-					   echo "<div class='cMatch'>Possible matches:</div>\n";
-				    }
-					$renderer->renderSearchResults($records);
-				}
-			}
-		}
-	} else {
-	    $renderer->renderError();
-	}
+    // Basic query, build filter and display results
+    if (strlen($_GET['q']) > 3) {
+        if (is_numeric(str_replace('-','',str_replace('(','',str_replace(')','',$_GET['q']))))) {
+            $records = $peepObj->getPhoneMatches($_GET['q']);
+            $renderer->renderSearchResults($records);
+        } else {
+            $records = $peepObj->getExactMatches($_GET['q']);
+            if (count($records) > 0) {
+                if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
+                    echo "<div class='cMatch'>Exact matches:</div>\n";
+                }
+                $renderer->renderSearchResults($records);
+            } else {
+                if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
+                    echo 'No exact matches found.';
+                }
+            }
+            if (count($records) < UNL_Peoplefinder::$displayResultLimit) {
+                // More room to display LIKE results
+                UNL_Peoplefinder::$displayResultLimit = UNL_Peoplefinder::$displayResultLimit-$peepObj->lastResultCount;
+                $records = $peepObj->getLikeMatches($_GET['q'], $records);
+                if (count($records) > 0) {
+                    if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
+                       echo "<div class='cMatch'>Possible matches:</div>\n";
+                    }
+                    $renderer->renderSearchResults($records);
+                }
+            }
+        }
+    } else {
+        $renderer->renderError();
+    }
 } elseif (isset($_GET['uid']) && !empty($_GET['uid'])) {
-	$renderer->renderRecord($peepObj->getUID($_GET['uid']));
+    $renderer->renderRecord($peepObj->getUID($_GET['uid']));
 } else {
     $renderer->renderError();
 }
