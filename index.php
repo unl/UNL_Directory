8d1cd674a922f8e4dc7b86472c364f8c83f3b03b
Brett Bieber <brett.bieber@gmail.com>
2009-11-18 00:31:40 +0000

    Remove session counter

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ $renderer = new UNL_Peoplefinder_Renderer_HTML($renderer_options);
 
 $myself = htmlentities(str_replace('index.php', '', $_SERVER['PHP_SELF']), ENT_QUOTES);
 
-if (!isset($_SESSION['lastResultDisplayed']))
-	$_SESSION['lastResultDisplayed']=0;
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
