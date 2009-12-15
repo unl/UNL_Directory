adca2dcb799fa9fc4fd5534cf1c01743facc77bd
Brett Bieber <brett.bieber@gmail.com>
2009-12-15 19:35:31 +0000

    Use Bieber instead of Brett Bieber so the differences can be seen

--- examples/getExactMatches.php
+++ examples/getExactMatches.php
@@ -3,7 +3,7 @@ require_once 'config.inc.php';
 
 $pf = new UNL_Peoplefinder();
 
-$results = $pf->getExactMatches('Brett Bieber');
+$results = $pf->getExactMatches('Bieber');
 
 foreach ($results as $uid) {
     echo $uid->cn.'<br />';
