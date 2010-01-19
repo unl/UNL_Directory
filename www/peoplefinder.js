5d64a6f94576adf6ebaca2159d9990520cb780a3
Brett Bieber <brett.bieber@gmail.com>
2010-01-19 22:02:17 +0000

    Use the value the user has entered.

--- www/peoplefinder.js
+++ www/peoplefinder.js
@@ -5,7 +5,7 @@ WDN.jQuery(document).ready(function() {
 	document.getElementById("form1").elements[0].focus();
 	WDN.loadJS('wdn/templates_3.0/scripts/toobar_peoplefinder.js');
 	WDN.jQuery('#form1').submit(function(eventObject) {
-		WDN.toolbar_peoplefinder.queuePFRequest('bieber', 'results');
+		WDN.toolbar_peoplefinder.queuePFRequest(WDN.jQuery('#q').val(), 'results');
 		eventObject.preventDefault();
 		eventObject.stopPropagation();
 		return false;
