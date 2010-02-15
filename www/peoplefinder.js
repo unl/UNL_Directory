055a607024a59c43d9d4225fc8202b659ab99e5f
Brett Bieber <brett.bieber@gmail.com>
2010-02-15 22:09:37 +0000

    Set the URL for the peoplefinder service.

--- www/peoplefinder.js
+++ www/peoplefinder.js
@@ -3,7 +3,9 @@
 WDN.jQuery(document).ready(function() {
 	self.focus();
 	document.getElementById("form1").elements[0].focus();
-	WDN.loadJS('wdn/templates_3.0/scripts/toolbar_peoplefinder.js');
+	WDN.loadJS('wdn/templates_3.0/scripts/toolbar_peoplefinder.js', function(){
+		WDN.toolbar_peoplefinder.serviceURL = '';
+	});
 	WDN.jQuery('#form1').submit(function(eventObject) {
 		WDN.toolbar_peoplefinder.queuePFRequest(WDN.jQuery('#q').val(), 'results');
 		eventObject.preventDefault();
