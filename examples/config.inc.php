f3968ecbb5da0f03a3762950d57d52114579c7da
Brett Bieber <brett.bieber@gmail.com>
2010-03-01 17:13:10 +0000

    Add example of looping through attributes returned.

--- examples/config.inc.php
+++ examples/config.inc.php
@@ -1,4 +1,4 @@
 <?php
 require_once 'UNL/Autoload.php';
 
-set_include_path(dirname(__FILE__).'/../src'.PATH_SEPARATOR.get_include_path());
+set_include_path(dirname(dirname(__FILE__)).'/src/'.PATH_SEPARATOR.dirname(dirname(__FILE__)).'/lib/php'.PATH_SEPARATOR.get_include_path());
--- examples/getUID.php
+++ examples/getUID.php
@@ -1,7 +1,14 @@
 <?php
 require_once 'config.inc.php';
 
-$pf = new UNL_Peoplefinder();
+$pf = new UNL_Peoplefinder($driver);
+
 $uid = $pf->getUID('bbieber2');
-echo 'uid:' . $uid->uid;
-echo ' cn:' . $uid->cn;
+
+echo 'uid:'   . $uid->uid;
+echo ' cn:'   . $uid->cn;
+echo ' mail:' . $uid->mail;
+echo ' department:' . $uid->unlHRPrimaryDepartment;
+foreach ($uid->eduPersonAffiliation as $affiliation) {
+    echo ' affiliation:' . $affiliation;
+}
