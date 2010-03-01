2b2985c98b8b772712389b94939d9adad92d624e
Brett Bieber <brett.bieber@gmail.com>
2010-03-01 20:26:44 +0000

    Make peoplefinder service return results in the same order as the public web interface. This contains major refactoring that needs to be tested by any app using peoplefinder as service.

--- www/service.php
+++ www/service.php
@@ -88,7 +88,9 @@ $renderer_class = 'UNL_Peoplefinder_Renderer_'.$renderer_class;
 $renderer = new $renderer_class($renderer_options);
 if (isset($_GET['q']) && !empty($_GET['q'])) {
     // Basic query, build filter and display results
-    if (strlen($_GET['q']) > 3) {
+    if (strlen($_GET['q']) <= 3) {
+        $renderer->renderError();
+    } else {
         if (is_numeric(str_replace('-','',str_replace('(','',str_replace(')','',$_GET['q']))))) {
             $records = $peepObj->getPhoneMatches($_GET['q'], $affiliation);
             $renderer->renderSearchResults($records);
@@ -101,32 +103,66 @@ if (isset($_GET['q']) && !empty($_GET['q'])) {
                     $renderer->renderError();
                 }
             } else {
+                // Standard text search
+                $by_affiliation = array();
+                $by_affiliation['faculty']       = array();
+                $by_affiliation['staff']         = array();
+                $by_affiliation['student']       = array();
+                $by_affiliation['organizations'] = array();
+                
+                $like_by_affiliation = $by_affiliation;
+                
                 $records = $peepObj->getExactMatches($_GET['q'], $affiliation);
-                if (count($records) > 0) {
-                    if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
-                        echo "<div class='cMatch'>Exact matches:</div>\n";
-                    }
-                    $renderer->renderSearchResults($records);
-                } else {
-                    if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
-                        echo 'No exact matches found.';
+                
+                UNL_Peoplefinder::$displayResultLimit -= count($records);
+                
+                $like_records = array();
+                if (UNL_Peoplefinder::$displayResultLimit) {
+                    $like_records = $peepObj->getLikeMatches($_GET['q'], $affiliation, $records);
+                }
+                
+                foreach(array('records'=>'by_affiliation', 'like_records'=>'like_by_affiliation') as $records_var=>$affiliation_var) {
+                    foreach ($$records_var as $record) {
+                        foreach ($record->ou as $ou) {
+                            if ($ou == 'org') {
+                                ${$affiliation_var}['organizations'][] = $record;
+                                break;
+                            }
+                        }
+
+                        if (isset($record->eduPersonAffiliation)) {
+                            foreach ($record->eduPersonAffiliation as $affiliation) {
+                                ${$affiliation_var}[$affiliation][] = $record;
+                            }
+                        }
                     }
                 }
-                if (count($records) < UNL_Peoplefinder::$displayResultLimit) {
-                    // More room to display LIKE results
-                    UNL_Peoplefinder::$displayResultLimit = UNL_Peoplefinder::$displayResultLimit - count($records);
-                    $records = $peepObj->getLikeMatches($_GET['q'], $affiliation, $records);
-                    if (count($records) > 0) {
+                
+                foreach ($by_affiliation as $affiliation=>$records) {
+                    if (count($records)
+                        || count($like_by_affiliation[$affiliation])) {
                         if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
-                           echo "<div class='cMatch'>Possible matches:</div>\n";
+                            echo '<div class="affiliation '.$affiliation.'">';
+                            echo '<h2>'.ucfirst($affiliation).'</h2>';
                         }
                         $renderer->renderSearchResults($records);
+                        if (count($like_by_affiliation[$affiliation])) {
+                            if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
+                                echo '<div class="likeResults">';
+                                echo '<h3>similar '.$affiliation.' results</h3>';
+                            }
+                            $renderer->renderSearchResults($like_by_affiliation[$affiliation]);
+                            if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
+                                echo '</div>';
+                            }
+                        }
+                        if ($renderer instanceof UNL_Peoplefinder_Renderer_HTML) {
+                            echo '</div>';
+                        }
                     }
                 }
             }
         }
-    } else {
-        $renderer->renderError();
     }
 } elseif (isset($_GET['uid']) && !empty($_GET['uid'])) {
     $renderer->renderRecord($peepObj->getUID($_GET['uid']));
