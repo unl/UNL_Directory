f3f79c034da30c3ee05463eb0cae6f9739c5e7eb
Seth Meranda <smeranda2@unl.edu>
2010-03-01 17:07:27 +0000

    hide the label when a search starts, bring it back if nothing entered.

--- www/scripts/peoplefinder.js
+++ www/scripts/peoplefinder.js
@@ -1,8 +1,6 @@
-
-
 WDN.jQuery(document).ready(function() {
-	self.focus();
-	document.getElementById("form1").elements[0].focus();
+	//self.focus();
+	//document.getElementById("form1").elements[0].focus();
 	WDN.loadJS('wdn/templates_3.0/scripts/toolbar_peoplefinder.js', function(){
 		WDN.toolbar_peoplefinder.serviceURL = '';
 	});
@@ -12,4 +10,23 @@ WDN.jQuery(document).ready(function() {
 		eventObject.stopPropagation();
 		return false;
 	});
+	WDN.jQuery('#q').click(hideLabel);
+	WDN.jQuery('#queryString').click(hideLabel);
+	if (WDN.jQuery('#q').val() !== "") {
+		hideLabel();
+	};
+	WDN.jQuery('#q').blur(function() {
+		if (WDN.jQuery('#q').val() === "") {
+			showLabel();
+		}
+	});
 });
+
+function hideLabel() {
+	WDN.jQuery('#queryString').hide();
+	WDN.jQuery('#q').focus();
+}
+
+function showLabel() {
+	WDN.jQuery('#queryString').show();
+}
