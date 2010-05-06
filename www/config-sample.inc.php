41f55a718e421e8e8e96b3abd4d2efab7737b777
Eric Rasmussen <erasmussen2@unl.edu>
2010-05-06 21:42:39 +0000

    Add autoloader

--- www/config-sample.inc.php
+++ www/config-sample.inc.php
@@ -1,6 +1,16 @@
 <?php
 ini_set('display_errors', true);
 error_reporting(E_ALL|E_STRICT);
+
+function autoload($class)
+{
+    $class = str_replace('_', '/', $class);
+    include $class . '.php';
+}
+    
+spl_autoload_register("autoload");
+
+
 set_include_path(dirname(dirname(__FILE__)).'/src/'.PATH_SEPARATOR.dirname(dirname(__FILE__)).'/lib/php'.PATH_SEPARATOR.get_include_path());
 require_once 'UNL/Autoload.php';
 
