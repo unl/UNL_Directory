5fc39ff1471abdeabf88ed2f09a855b09ba95e7a
Brett Bieber <brett.bieber@gmail.com>
2010-07-13 13:44:03 +0000

    Remove unused pagination links.

--- www/templates/html/Peoplefinder/SearchResults.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults.tpl.php
@@ -9,24 +9,8 @@ if (($start+$num_rows)>count($context)) {
 } else {
     $end = $start+$num_rows;
 }
-if ($start > 0 || $end < count($context)) {
-    //Display Page information
-    $page = (isset($context->options['p']))?$context->options['p']:0;
-    $next = $page + 1;
-    if ($page>=1)  {
-        $prevLink = '<a class="previous" href="'.UNL_Peoplefinder::getURL().'?'.preg_replace('/[&]?p=\d/','',$_SERVER['QUERY_STRING']).'&amp;p='.($page-1).'">&lt;&lt;&nbsp;</a>';
-    } else {
-        $prevLink = '&lt;&lt;&nbsp;';
-    }
-    if ($end < $num_records) {
-        $nextLink = "<a class='next' href='".UNL_Peoplefinder::getURL()."?".preg_replace("/[&]?p=\d/","",$_SERVER['QUERY_STRING'])."&amp;p=$next'>&nbsp;&gt;&gt;</a>";
-    }
-    else $nextLink = '&nbsp;&gt;&gt;';
-    $navlinks = '<div class="cNav">'.$prevLink.$nextLink.'</div>';
-} else {
-    $navlinks = '';
-}
-echo "<div class='result_head'>Results ".($start+1)." - $end out of ".count($context).':'.$navlinks.'</div>'.PHP_EOL;
+
+echo "<div class='result_head'>Results ".($start+1)." - $end out of ".count($context).'</div>'.PHP_EOL;
 echo '<ul class="pfResult">'.PHP_EOL; //I need to put a class for CSS, however when we switch to chuncked results (student, staff, faculty) this @todo will need revisted
 for ($i = $start; $i<$end; $i++) {
     $even_odd = ($i % 2) ? '' : 'alt';
@@ -43,7 +27,6 @@ for ($i = $start; $i<$end; $i++) {
     echo '</li>'.PHP_EOL;
 }
 echo '</ul>'.PHP_EOL;
-echo "<div class='result_head'>$navlinks</div>";
 
 if (count($context) >= UNL_Peoplefinder::$resultLimit) {
     echo "<p>Your search could only return a subset of the results. ";
