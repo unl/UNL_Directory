142eb53bfde7c99b66ae1024d24338cdb8f4e0d5
Brett Bieber <brett.bieber@gmail.com>
2010-07-14 17:31:27 +0000

    Whitespace fixes

--- www/templates/html/Officefinder/StandardForm.tpl.php
+++ www/templates/html/Officefinder/StandardForm.tpl.php
@@ -1,12 +1,12 @@
 <form method="get" action="?" id="officefinder" class="directorySearch">
-	<fieldset>
-	    <legend>Search for departments</legend>
-	<ol>
-	    <li>
-		    <label for="q2" id="queryString2">Enter a name to begin your search</label> 
-		    <input type="text" value="<?php echo $context->options['q']; ?>" id="q2" name="q" />
-		    <input name="submitbutton" type="image" src="images/formSearch.png" value="Submit" />
-	    </li>
-	</ol>
-	</fieldset>
+    <fieldset>
+        <legend>Search for departments</legend>
+    <ol>
+        <li>
+            <label for="q2" id="queryString2">Enter a name to begin your search</label> 
+            <input type="text" value="<?php echo $context->options['q']; ?>" id="q2" name="q" />
+            <input name="submitbutton" type="image" src="images/formSearch.png" value="Submit" />
+        </li>
+    </ol>
+    </fieldset>
 </form>
