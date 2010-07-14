c24568e37bafa869cf17dd52fe373bf99258e914
Brett Bieber <brett.bieber@gmail.com>
2010-07-14 16:10:43 +0000

    Display address next to listings.

--- www/templates/html/Officefinder/Department/Listing.tpl.php
+++ www/templates/html/Officefinder/Department/Listing.tpl.php
@@ -1,5 +1,6 @@
 <?php
-echo $context->name.' '.$savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php');;
+echo $context->name.' '.$savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php');
+echo ' '.$context->address;
 
 if ($context->userCanEdit(UNL_Officefinder::getUser())) {
     echo ' <a href="?view=listing&amp;id='.$context->id.'&amp;format=editing">Edit</a>';
