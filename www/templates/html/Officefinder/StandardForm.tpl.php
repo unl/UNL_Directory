e87ac5d3d316590db87020b2c77a3e8a59f3a6a6
Brett Bieber <brett.bieber@gmail.com>
2010-07-14 17:32:55 +0000

    Fix undefined variable notice

--- www/templates/html/Officefinder/StandardForm.tpl.php
+++ www/templates/html/Officefinder/StandardForm.tpl.php
@@ -3,8 +3,15 @@
         <legend>Search for departments</legend>
     <ol>
         <li>
-            <label for="q2" id="queryString2">Enter a name to begin your search</label> 
-            <input type="text" value="<?php echo $context->options['q']; ?>" id="q2" name="q" />
+            <label for="q2" id="queryString2">Enter a name to begin your search</label>
+            <?php
+            if (isset($context->options['q'])) {
+                $default = htmlentities($context->options['q'], ENT_QUOTES);
+            } else {
+                $default = '';
+            }
+            ?>
+            <input type="text" value="<?php echo $default; ?>" id="q2" name="q" />
             <input name="submitbutton" type="image" src="images/formSearch.png" value="Submit" />
         </li>
     </ol>
