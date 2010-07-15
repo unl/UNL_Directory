86705e86d5626f451c68531030fc1f1ea3c5a8fc
Seth Meranda <smeranda2@unl.edu>
2010-07-15 15:02:06 +0000

    style the department listings

--- www/templates/html/Officefinder/DepartmentList/NameSearch.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/NameSearch.tpl.php
@@ -1,10 +1,24 @@
+<div class="affiliation">
+<h2>Departments</h2>
 <?php
 if (count($context)) {
     echo '<div class="result_head">'.count($context).' result(s) found</div>';
-    echo '<ul>';
+    echo '<ul class="pfResult">';
     foreach ($context as $department) {
-        echo '<li><a href="'.UNL_Peoplefinder::getURL().'departments/?view=department&amp;id='.$department->id.'">'.$department->name.'</a></li>';
-    }
+        echo '<li>
+                <div class="overflow">
+	                <a class="planetred_profile" href="?view=department&amp;id='.$department->id.'">
+	                    <img alt="Generic Icon" src="images/organization40.png" class="profile_pic small photo">
+	                </a>
+	                <div class="recordDetails">
+		                <div class="fn">
+		                    <a href="'.UNL_Peoplefinder::getURL().'departments/?view=department&amp;id='.$department->id.'">'.$department->name.'</a>
+		                </div>
+	                </div>
+                    <a class="cInfo"" href="'.UNL_Peoplefinder::getURL().'departments/?view=department&amp;id='.$department->id.'">More Details</a>
+                </div>
+             </li>';
     echo '</ul>';
 }
-
+?>
+</div>
