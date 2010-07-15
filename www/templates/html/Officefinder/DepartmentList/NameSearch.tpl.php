bbe7dc562476492112a81a051372d541fdb190d0
Brett Bieber <brett.bieber@gmail.com>
2010-07-15 14:40:39 +0000

    Correct link to department result pages.

--- www/templates/html/Officefinder/DepartmentList/NameSearch.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/NameSearch.tpl.php
@@ -3,7 +3,7 @@ if (count($context)) {
     echo '<div class="result_head">'.count($context).' result(s) found</div>';
     echo '<ul>';
     foreach ($context as $department) {
-        echo '<li><a href="?view=department&amp;id='.$department->id.'">'.$department->name.'</a></li>';
+        echo '<li><a href="'.UNL_Peoplefinder::getURL().'departments/?view=department&amp;id='.$department->id.'">'.$department->name.'</a></li>';
     }
     echo '</ul>';
 }
