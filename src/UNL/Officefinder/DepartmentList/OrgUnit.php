0ce997307508852b15b3362ffedf3bfb0887ba2f
Brett Bieber <brett.bieber@gmail.com>
2010-07-15 14:43:03 +0000

    Add object for searching by org_unit

---
+++ src/UNL/Officefinder/DepartmentList/OrgUnit.php
@@ -0,0 +1,23 @@
+<?php
+class UNL_Officefinder_DepartmentList_OrgUnit extends UNL_Officefinder_DepartmentList
+{
+    public $options = array('q'=>'');
+
+    function __construct($options = array())
+    {
+        $this->options = $options + $this->options;
+        $records = array();
+        $mysqli = UNL_Officefinder::getDB();
+        $sql = 'SELECT id FROM departments ';
+        $sql .= 'WHERE org_unit = "'.$mysqli->escape_string($this->options['q']).'"'
+             . ' ORDER BY name';
+        if ($result = $mysqli->query($sql)) {
+            while($row = $result->fetch_array(MYSQLI_NUM)) {
+                $records[] = $row[0];
+            }
+        }
+        $mysqli->close();
+        parent::__construct($records);
+    }
+
+}
