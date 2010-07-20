a3c6951cfb8c8ee71ec7bfb0b2acd244c4c912c1
Seth Meranda <smeranda2@unl.edu>
2010-07-20 14:42:21 +0000

    fn org is the correct microformat

--- www/templates/html/Officefinder/Department.tpl.php
+++ www/templates/html/Officefinder/Department.tpl.php
@@ -1,27 +1,29 @@
-<h2 class="fn"><?php echo $context->name; ?></h2>
-<img alt="Generic Icon" src="http://www1.unl.edu/tour/BuildingImages/<?php echo $context->building;?>_01_sm.jpg" width="100" height="100" class="frame photo">
-<div class="vcardInfo">
-	<div class="adr">
-	     <span class="street-address"><?php echo $context->address; ?></span>
-	     <span class="room"><?php echo $context->room.' <a class="location mapurl" href="http://www1.unl.edu/tour/'.$context->building.'">'.$context->building.'</a>'; ?></span>
-	     <span class="locality"><?php echo $context->city; ?></span>
-	     <span class="region"><?php echo $context->state; ?></span>
-	     <span class="postal-code"><?php echo $context->postal_code; ?><?php echo $context->email; ?></span>
-	     <div class="country-name">USA</div>
+<div class="vcard"></div>
+	<h2 class="fn org"><?php echo $context->name; ?></h2>
+	<img alt="Generic Icon" src="http://www1.unl.edu/tour/BuildingImages/<?php echo $context->building;?>_01_sm.jpg" width="100" height="100" class="frame photo">
+	<div class="vcardInfo">
+		<div class="adr label">
+		     <span class="street-address"><?php echo $context->address; ?></span>
+		     <span class="room"><?php echo $context->room.' <a class="location mapurl" href="http://www1.unl.edu/tour/'.$context->building.'">'.$context->building.'</a>'; ?></span>
+		     <span class="locality"><?php echo $context->city; ?></span>
+		     <span class="region"><?php echo $context->state; ?></span>
+		     <span class="postal-code"><?php echo $context->postal_code; ?><?php echo $context->email; ?></span>
+		     <div class="country-name">USA</div>
+		</div>
+		<div class="tel">
+		    <span class="value">
+				<?php
+				echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php');
+				?>
+		    </span>
+		</div>
+		<span class="email">
+		   <a class="email" href="mailto:<?php echo $context->email; ?>"><?php echo $context->email; ?></a>
+		</span>
+		<span class="url">
+		   <a class="url" href="<?php echo $context->website; ?>"><?php echo $context->website; ?></a>
+		</span>
 	</div>
-	<div class="tel">
-	    <span class="value">
-			<?php
-			echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php');
-			?>
-	    </span>
-	</div>
-	<span class="email">
-	   <a class="email" href="mailto:<?php echo $context->email; ?>"><?php echo $context->email; ?></a>
-	</span>
-	<span class="url">
-	   <a class="url" href="<?php echo $context->website; ?>"><?php echo $context->website; ?></a>
-	</span>
 </div>
 <div class="clear"></div>
 <?php 
