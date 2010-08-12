59db4928252746474111f6cb8358160bff25e4e9
Brett Bieber <brett.bieber@gmail.com>
2010-08-12 20:45:06 +0000

    Include the number of children that will be deleted in the warning message.

--- www/templates/editing/Officefinder/Department/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/DeleteForm.tpl.php
@@ -1,5 +1,5 @@
 <form action="<?php echo UNL_Officefinder::getURL(); ?>?view=department&amp;id=<?php echo $context->id; ?>" method="post" id="deletedepartment_<?php echo $context->id; ?>" style="width:120px;">
     <input type="hidden" name="_type" value="delete_department" />
     <input type="hidden" name="department_id" value="<?php echo $context->id; ?>" />
-    <a href="#" onclick="if (confirm('Are you sure?')) document.getElementById('deletedepartment_<?php echo $context->id; ?>').submit();">Delete</a>
+    <a href="#" onclick="if (confirm('Are you sure? This will delete this record and <?php echo count($context->getChildren()); ?> children.')) document.getElementById('deletedepartment_<?php echo $context->id; ?>').submit();">Delete</a>
 </form>
