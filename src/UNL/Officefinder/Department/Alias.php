cf5703e3596e2f894b1ab6d19b0b53b9782f7fe6
Brett Bieber <brett.bieber@gmail.com>
2010-08-20 20:37:15 +0000

    Add getById method.

--- src/UNL/Officefinder/Department/Alias.php
+++ src/UNL/Officefinder/Department/Alias.php
@@ -13,4 +13,17 @@ class UNL_Officefinder_Department_Alias extends UNL_Officefinder_Record
     {
         return array('department_id', 'name');
     }
+
+    public static function getById($department_id, $name)
+    {
+        $mysqli = UNL_Officefinder::getDB();
+        $sql = "SELECT * FROM department_aliases WHERE department_id = ".intval($department_id)." AND name = '".$mysqli->escape_string($name)."'";
+        if (($result = $mysqli->query($sql))
+            && $result->num_rows > 0) {
+            $object = new self();
+            UNL_Officefinder::setObjectFromArray($object, $result->fetch_assoc());
+            return $object;
+        }
+        return false;
+    }
 }
