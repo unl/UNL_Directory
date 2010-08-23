454e1e5c60d3376c56643f93c2cab3f74986ce00
Brett Bieber <brett.bieber@gmail.com>
2010-08-23 16:06:15 +0000

    Remove sort field for now. Need to figure out a better way to do this.

--- www/templates/html/Officefinder/Department/Listing/SortForm.tpl.php
+++ www/templates/html/Officefinder/Department/Listing/SortForm.tpl.php
@@ -1,5 +1,4 @@
 <form method="post" action="<?php echo UNL_Officefinder::getURL(); ?>?view=department&amp;id=<?php echo $context->department_id; ?>">
     <input type="hidden" name="_type" value="listing" />
     <input type="hidden" name="id" value="<?php echo $context->id; ?>" />
-    <input type="hidden" name="sort" value="<?php echo $context->sort; ?>" />
 </form>
