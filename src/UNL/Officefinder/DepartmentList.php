cebf37c56af2ed73949ecfb87ea0c121c21fcfe8
Brett Bieber <brett.bieber@gmail.com>
2010-08-23 21:54:42 +0000

    Allow an array to be synchronized from.

--- src/UNL/Officefinder/DepartmentList.php
+++ src/UNL/Officefinder/DepartmentList.php
@@ -3,6 +3,13 @@ class UNL_Officefinder_DepartmentList extends ArrayIterator
 {
     function current()
     {
-        return UNL_Officefinder_Department::getByID(parent::current());
+        $current = parent::current();
+        if (gettype($current) == 'string') {
+            return UNL_Officefinder_Department::getByID($current);
+        }
+
+        $dept = new UNL_Officefinder_Department();
+        $dept->synchronizeWithArray($current);
+        return $dept;
     }
 }
