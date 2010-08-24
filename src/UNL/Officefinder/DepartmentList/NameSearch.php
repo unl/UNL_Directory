50d7b4867f7057e8f7641e56a7a4d390e1ac7a33
Brett Bieber <brett.bieber@gmail.com>
2010-08-24 18:15:56 +0000

    Add in sample styling for grabbing official parent org information and displaying matching listing/sub-dept.

--- src/UNL/Officefinder/DepartmentList/NameSearch.php
+++ src/UNL/Officefinder/DepartmentList/NameSearch.php
@@ -1,18 +1,20 @@
 <?php
 class UNL_Officefinder_DepartmentList_NameSearch extends UNL_Officefinder_DepartmentList
 {
-    public $options = array('q'=>'');
+    public $options = array('q'=>'', 'parent_orgs' => true);
 
     function __construct($options = array())
     {
         $this->options = $options + $this->options;
         $records = array();
         $mysqli = UNL_Officefinder::getDB();
-        $sql = 'SELECT DISTINCT departments.id FROM departments, department_aliases ';
-        $sql .= 'WHERE (departments.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%" OR
-                    (department_aliases.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%" AND department_aliases.department_id = departments.id))'
-             . ' AND (departments.rgt != departments.lft+1 OR departments.org_unit IS NOT NULL) '
-             . ' ORDER BY departments.name';
+        $sql = 'SELECT DISTINCT departments.id FROM departments, department_aliases
+                WHERE (departments.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%" OR
+                    (department_aliases.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%" AND department_aliases.department_id = departments.id))';
+        if ((bool)$this->options['parent_orgs'] === true) {
+             $sql .= ' AND (departments.rgt != departments.lft+1 OR departments.org_unit IS NOT NULL) ';
+        }
+        $sql .= ' ORDER BY departments.name';
         if ($result = $mysqli->query($sql)) {
             while($row = $result->fetch_array(MYSQLI_NUM)) {
                 $records[] = $row[0];
--- www/templates/html/Officefinder/DepartmentList/NameSearch.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/NameSearch.tpl.php
@@ -5,6 +5,13 @@ if (count($context)) {
     echo '<div class="result_head">'.count($context).' result(s) found</div>';
     echo '<ul class="pfResult departments">';
     foreach ($context as $department) {
+
+        $title = '';
+//        if (!$department->isOfficialDepartment()) {
+//            $sub_dept   = $department;
+//            $department = $sub_dept->getOfficialParent();
+//            $title      = '<div class="title">'.$sub_dept->name.'</div>';
+//        }
         echo '<li>
                 <div class="overflow">
                     <a class="planetred_profile" href="?view=department&amp;id='.$department->id.'">
@@ -14,6 +21,7 @@ if (count($context)) {
                         <div class="fn">
                             <a href="'.UNL_Officefinder::getURL().'?view=department&amp;id='.$department->id.'">'.$department->name.'</a>
                         </div>
+                        '.$title.'
                     </div>
                     <a class="cInfo"" href="'.UNL_Officefinder::getURL().'?view=department&amp;id='.$department->id.'" onclick="return service_officefinder.of_getUID(\''.$department->id.'\');">More Details</a>
                 </div>
