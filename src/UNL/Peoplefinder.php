4bab43e108ddce068e834cfd733f007448df0409
Seth Meranda <smeranda2@unl.edu>
2010-08-24 21:31:36 +0000

    revert to case sensitive matching

--- src/UNL/Peoplefinder.php
+++ src/UNL/Peoplefinder.php
@@ -76,12 +76,12 @@ class UNL_Peoplefinder
         if (isset($_SERVER['HTTP_ACCEPT'])
             && $this->options['format'] == 'html' && (
                 ($this->options['mobile'] !== false && $this->options['mobile'] != 'no')
-                || (preg_match('/text\/vnd\.wap\.wml|application\/vnd\.wap\.xhtml\+xml/i', $_SERVER['HTTP_ACCEPT']))
-                    || preg_match('/sony|symbian|nokia|samsung|mobile|windows ce|epoc|opera/i', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/mini|nitro|j2me|midp-|cldc-|netfront|mot|up\.browser|up\.link|audiovox/i', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/blackberry|ericsson,|panasonic|philips|sanyo|sharp|sie-/i', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/portalmmm|blazer|avantgo|danger|palm|series60|palmsource|pocketpc/i', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/smartphone|rover|ipaq|au-mic,|alcatel|ericy|vodafone\/|wap1\.|wap2\.|iphone|android/i', $_SERVER['HTTP_USER_AGENT'])
+                || (preg_match('/text\/vnd\.wap\.wml|application\/vnd\.wap\.xhtml\+xml/', $_SERVER['HTTP_ACCEPT']))
+                    || preg_match('/sony|symbian|nokia|samsung|mobile|windows ce|epoc|opera/', $_SERVER['HTTP_USER_AGENT'])
+                    || preg_match('/mini|nitro|j2me|midp-|cldc-|netfront|mot|up\.browser|up\.link|audiovox/', $_SERVER['HTTP_USER_AGENT'])
+                    || preg_match('/blackberry|ericsson,|panasonic|philips|sanyo|sharp|sie-/', $_SERVER['HTTP_USER_AGENT'])
+                    || preg_match('/portalmmm|blazer|avantgo|danger|palm|series60|palmsource|pocketpc/', $_SERVER['HTTP_USER_AGENT'])
+                    || preg_match('/smartphone|rover|ipaq|au-mic,|alcatel|ericy|vodafone\/|wap1\.|wap2\.|iphone|Android/', $_SERVER['HTTP_USER_AGENT'])
             )) {
             $this->options['mobile'] = true;
         }
