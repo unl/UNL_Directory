e13deb89807fd3f06c7db76f6a54fc07d4ce2849
Brett Bieber <brett.bieber@gmail.com>
2010-08-24 21:41:53 +0000

    Check if the user agent var is set to prevent notice.

--- www/templates/html/Peoplefinder/Record/TelephoneNumber.tpl.php
+++ www/templates/html/Peoplefinder/Record/TelephoneNumber.tpl.php
@@ -1,6 +1,7 @@
 <?php
 $link = '<a href="';
-if (strpos($_SERVER['HTTP_USER_AGENT'], "iPhone") === false) {
+if (isset($_SERVER['HTTP_USER_AGENT']) &&
+    strpos($_SERVER['HTTP_USER_AGENT'], "iPhone") === false) {
     $link .= "wtai://wp/mc;".str_replace(array("(", ")", "-"), "", $context);
 } else {
     $link .= "tel:".$context;
