ee1cfe03678db5db8b03bc15cbd263dac9b12e2b
Brett Bieber <brett.bieber@gmail.com>
2010-08-26 13:03:14 +0000

    Whitespace/indentation for readability

--- www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
@@ -1,20 +1,19 @@
 <div id="filters" class="one_col left">
-<div class="zenbox energetic wdn_filterset">
-    <h3>Filter People Results</h3>
-    <form method="post" action="#" class="filters">
-
-    <fieldset class="affiliation">
-        <legend><span>By Affiliation</span></legend>
-        <ol>
-           <li>
-           </li>
-        </ol>
-    </fieldset>
-    <fieldset class="department">
-        <legend><span>By Department</span></legend>
-        <ol>
-        </ol>
-    </fieldset>
-    </form>
-</div>
+    <div class="zenbox energetic wdn_filterset">
+        <h3>Filter People Results</h3>
+        <form method="post" action="#" class="filters">
+            <fieldset class="affiliation">
+                <legend><span>By Affiliation</span></legend>
+                <ol>
+                   <li>
+                   </li>
+                </ol>
+            </fieldset>
+            <fieldset class="department">
+                <legend><span>By Department</span></legend>
+                <ol>
+                </ol>
+            </fieldset>
+        </form>
+    </div>
 </div>
