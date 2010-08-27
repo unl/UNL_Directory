8dff570df19e5b5b368d11fb8a874e4d3c89679c
Brett Bieber <brett.bieber@gmail.com>
2010-08-27 21:29:22 +0000

    Add mock driver for peoplefinder - for use when you have no Internet connection.

---
+++ src/UNL/Peoplefinder/Driver/Mock.php
@@ -0,0 +1,33 @@
+<?php
+class UNL_Peoplefinder_Driver_Mock implements UNL_Peoplefinder_DriverInterface
+{
+    function getAdvancedSearchMatches($query, $affiliation = null)
+    {
+        return array();
+    }
+
+    function getExactMatches($query, $affiliation = null)
+    {
+        return array();
+    }
+
+    function getLikeMatches($query, $affiliation = null)
+    {
+        return array();
+    }
+
+    function getPhoneMatches($query, $affiliation = null)
+    {
+        return array();
+    }
+
+    function getUID($uid)
+    {
+        return false;
+    }
+
+    function getHRPrimaryDepartmentMatches($query, $affiliation = null)
+    {
+        return array();
+    }
+}
