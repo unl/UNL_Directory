4c8f3abf13fdb4c429d19bdc703e54aa8762c1ca
Brett Bieber <brett.bieber@gmail.com>
2010-09-01 18:50:16 +0000

    Link up building codes if the address begins with a building code.

--- www/templates/html/Officefinder/Department/Listing.tpl.php
+++ www/templates/html/Officefinder/Department/Listing.tpl.php
@@ -1,4 +1,9 @@
 <?php
+$address = $context->address;
+if (preg_match('/^([A-Z]+)/', $context->address, $matches)) {
+    $address = str_replace($matches[1], '<a class="location mapurl" href="http://www1.unl.edu/tour/'.$matches[1].'">'.$matches[1].'</a>', $context->address);
+}
+
 echo '<div class="edit">';
 if ($context->userCanEdit(UNL_Officefinder::getUser())) {
     echo ' <a href="'.UNL_Officefinder::getURL().'?view=department&amp;id='.$context->id.'&amp;format=editing" class="action edit">Edit</a>'.PHP_EOL;
@@ -9,6 +14,6 @@ echo '</div>';
 
 echo '<div class="listingDetails">';
 echo $context->name.' <span class="tel">'.$savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php').'</span>';
-echo ' <span class="adr">'.$context->address.'</span>';
+echo ' <span class="adr">'.$address.'</span>';
 echo '</div>';
-
+?>
