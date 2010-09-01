9d22f78ed1823dc10ebae5c9843a1e67e25413a1
Brett Bieber <brett.bieber@gmail.com>
2010-09-01 16:03:19 +0000

    Add newline for readability

--- www/templates/html/Officefinder/Department/Listing.tpl.php
+++ www/templates/html/Officefinder/Department/Listing.tpl.php
@@ -1,7 +1,7 @@
 <?php
 echo '<div class="edit">';
 if ($context->userCanEdit(UNL_Officefinder::getUser())) {
-    echo ' <a href="'.UNL_Officefinder::getURL().'?view=department&amp;id='.$context->id.'&amp;format=editing" class="action edit">Edit</a>';
+    echo ' <a href="'.UNL_Officefinder::getURL().'?view=department&amp;id='.$context->id.'&amp;format=editing" class="action edit">Edit</a>'.PHP_EOL;
     echo $savvy->render($context, 'Officefinder/Department/Listing/SortForm.tpl.php');
     include dirname(__FILE__).'/../../../editing/Officefinder/Department/DeleteForm.tpl.php';
 }
