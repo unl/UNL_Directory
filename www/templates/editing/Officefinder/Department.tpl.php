1d3301f3863c45893411f5635f378020a343b396
Brett Bieber <brett.bieber@gmail.com>
2010-09-10 19:15:41 +0000

    Convert department details form to a zenform.

--- www/templates/editing/Officefinder/Department.tpl.php
+++ www/templates/editing/Officefinder/Department.tpl.php
@@ -1,24 +1,52 @@
-<form method="post" action="<?php echo $context->getURL(); ?>">
-<input type="hidden" name="_type" value="department" />
-<?php
-foreach ($context as $var=>$value) {
-    switch($var) {
-        case 'id':
-            $type = 'hidden';
-            break;
-        case 'uidlastupdated':
-        case 'options':
-        case 'lft':
-        case 'rgt':
-        case 'level':
-            continue 2;
-        default:
-            $type = 'text';
-    }
-    echo $var . ': <input type="'.$type.'" name="'.$var.'" value="'.$value.'" /><br />'.PHP_EOL;
-}
-?>
-    <input type="submit" />
+<script type="text/javascript">
+//<![CDATA[
+    WDN.jQuery(document).ready(function(){
+         WDN.initializePlugin('zenform');
+    });
+//]]>
+</script>
+<h3 class="zenform">Edit Department Record</h3>
+<form class="zenform" action="<?php echo $context->getURL(); ?>" method="post">
+    <fieldset>
+        <legend>Department Details</legend>
+        <ol>
+            <li>
+                <label for="name">
+                    <span class="required">*</span>
+                    Name
+                    <span class="helper">The official name of this entity</span>
+                </label>
+                <input type="text" id="name" name="name" value="<?php echo $context->name; ?>" />
+            </li>
+            <li>
+                <label for="building">Campus Building</label>
+                <select name="building" id="building">
+                    <option value="">N/A</option>
+                    <?php
+                    $buildings = new UNL_Common_Building();
+                    foreach ($buildings->codes as $code=>$name):
+                    $selected = '';
+                    if ($code == $context->building) {
+                        $selected = 'selected="selected"';
+                    }
+                    ?>
+                    <option value="<?php echo $code; ?>" <?php echo $selected; ?>><?php echo "$name ($code)"; ?></option>
+                    <?php endforeach;
+                    if ($buildings->buildingExists($context->building)) {
+                        echo '<option value="'.$context->building.'">'.$context->building.' (Unknown)</option>'.PHP_EOL;
+                    }
+                    ?>
+                </select>
+            </li>
+            <?php foreach (array('room', 'city', 'state', 'postal_code', 'phone', 'fax', 'email', 'website') as $var): ?>
+            <li>
+                <label for="<?php echo $var; ?>"><?php echo ucwords(str_replace('_', ' ', $var)); ?></label>
+                <input type="text" id="<?php echo $var; ?>" name="<?php echo $var; ?>" value="<?php echo $context->$var; ?>" />
+            </li>
+            <?php endforeach; ?>
+        </ol>
+    </fieldset>
+    <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
 // Disable editing sublistings for now.
