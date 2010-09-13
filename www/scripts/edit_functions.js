25e832f67047361865717dff61d3a57ef5543c44
Seth Meranda <smeranda2@unl.edu>
2010-09-13 17:00:47 +0000

    all edit links, not just the listings.

--- www/scripts/edit_functions.js
+++ www/scripts/edit_functions.js
@@ -22,7 +22,7 @@ WDN.jQuery(document).ready(function(){
 		WDN.log(href);
 		WDN.jQuery(this).attr('href', href.replace('format=editing', 'format[]=editing&format[]=partial'));
 	});
-	WDN.jQuery('ul.listings a.edit').colorbox({width: '75%', height: '75%'});
+	WDN.jQuery('a.edit').colorbox({width: '75%', height: '75%'});
 });
 
 function saveSortOrder(list) {//this function determines the order of the list and sends it to the DB.
