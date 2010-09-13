f3621eac21500192a3909317a383d2cd45051967
Seth Meranda <smeranda2@unl.edu>
2010-09-13 14:36:20 +0000

    lowercase the text.

--- www/templates/editing/Officefinder/Department/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/DeleteForm.tpl.php
@@ -1,4 +1,4 @@
-<a href="#" onclick="if (confirm('Are you sure? This will delete this record and <?php echo count($context->getChildren()); ?> children.')) document.getElementById('deletedepartment_<?php echo $context->id; ?>').submit();">Delete</a>
+<a href="#" onclick="if (confirm('Are you sure? This will delete this record and <?php echo count($context->getChildren()); ?> children.')) document.getElementById('deletedepartment_<?php echo $context->id; ?>').submit();">delete</a>
 <form action="<?php echo $context->getURL(); ?>" method="post" id="deletedepartment_<?php echo $context->id; ?>" style="width:120px;">
     <input type="hidden" name="_type" value="delete_department" />
     <input type="hidden" name="department_id" value="<?php echo $context->id; ?>" />
