0a25b45c4dd9d45a4a8ac72d5e22dc1082a6b856
Brett Bieber <brett.bieber@gmail.com>
2010-09-13 17:06:10 +0000

    Allow user to cancel delete and stay where they are.

--- www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
@@ -1,4 +1,4 @@
-<a href="#" onclick="if (confirm('Are you sure? This will delete the alias.')) document.getElementById('deletealias_<?php echo $context->department_id.$context->name; ?>').submit();">Delete</a>
+<a href="#" onclick="if (confirm('Are you sure? This will delete the alias.')) document.getElementById('deletealias_<?php echo $context->department_id.$context->name; ?>').submit(); else return false;">Delete</a>
 <form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deletealias_<?php echo $context->department_id.$context->name; ?>" style="width:120px;">
     <input type="hidden" name="_type" value="delete_dept_alias" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
