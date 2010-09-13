c9b6f6e38b09776655dcd5d11022a072dc907452
Brett Bieber <brett.bieber@gmail.com>
2010-09-13 16:42:06 +0000

    Improve selector used for determining the sort form so the correct form is posted.

--- www/scripts/edit_functions.js
+++ www/scripts/edit_functions.js
@@ -2,7 +2,7 @@ WDN.jQuery(document).ready(function(){
 	WDN.loadJS('/wdn/templates_3.0/scripts/plugins/jeditable/jquery.jeditable.js', function(){
 		
 	});
-	WDN.jQuery('ul.sortable').sortable({ //make all the lists on the edit interface sortable
+	WDN.jQuery('ul.listings').sortable({ //make all the lists on the edit interface sortable
 		revert: false,
 		scroll: true,
 		delay: 100,
@@ -25,9 +25,9 @@ function saveSortOrder(list) {//this function determines the order of the list a
 	var results = WDN.jQuery(list).sortable('toArray');
 
 	for (i = 0; i<results.length; i++) {
-		if (WDN.jQuery('#'+results[i]+' form input[name=sort_order]').attr('value') != i+1) {
-			WDN.jQuery('#'+results[i]+' form input[name=sort_order]').attr('value', i+1);
-			WDN.jQuery.post(WDN.jQuery('#'+results[i]+' form').attr('action'), WDN.jQuery('#'+results[i]+' form').serialize());
+		if (WDN.jQuery('#'+results[i]+' form.sortform input[name=sort_order]').attr('value') != i+1) {
+			WDN.jQuery('#'+results[i]+' form.sortform input[name=sort_order]').attr('value', i+1);
+			WDN.jQuery.post(WDN.jQuery('#'+results[i]+' form.sortform').attr('action'), WDN.jQuery('#'+results[i]+' form.sortform').serialize());
 		}
 	}
 }
--- www/templates/html/Officefinder/Department/Listing/SortForm.tpl.php
+++ www/templates/html/Officefinder/Department/Listing/SortForm.tpl.php
@@ -1,4 +1,4 @@
-<form method="post" action="<?php echo UNL_Officefinder::getURL(); ?>?view=department&amp;id=<?php echo $context->parent_id; ?>">
+<form method="post" action="<?php echo UNL_Officefinder::getURL(); ?>?view=department&amp;id=<?php echo $context->parent_id; ?>" class="sortform">
     <input type="hidden" name="_type" value="department" />
     <input type="hidden" name="id" value="<?php echo $context->id; ?>" />
     <input type="hidden" name="sort_order" value="<?php echo $context->sort_order; ?>" />
