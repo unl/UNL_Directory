2c3a3b51be5a3f7830242b0f90abd50d797f78ff
Brett Bieber <brett.bieber@gmail.com>
2010-09-20 18:37:04 +0000

    Remove unused constant, add documentation, improve organization.

--- www/config-sample.inc.php
+++ www/config-sample.inc.php
@@ -14,23 +14,25 @@ spl_autoload_register("autoload");
 set_include_path(dirname(dirname(__FILE__)).'/src/'.PATH_SEPARATOR.dirname(dirname(__FILE__)).'/lib/php');
 require_once 'UNL/Autoload.php';
 
-define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
-
-UNL_Peoplefinder_Driver_LDAP::$bindDN = 'uid=giggidy,ou=service,dc=unl,dc=edu';
-UNL_Peoplefinder_Driver_LDAP::$bindPW = 'flibbertygibberty';
-
+// Set the main URL for the site
 UNL_Peoplefinder::$url = 'http://peoplefinder.unl.edu/';
+
+// Some LDAP queries take a long time, change this if necessary
 set_time_limit(5);
 
-// If you have LDAP access credentials, best to use this driver
+// If you have LDAP access credentials, best to use this driver, using your credentials
+UNL_Peoplefinder_Driver_LDAP::$bindDN = 'uid=giggidy,ou=service,dc=unl,dc=edu';
+UNL_Peoplefinder_Driver_LDAP::$bindPW = 'flibbertygibberty';
 $driver = new UNL_Peoplefinder_Driver_LDAP();
 
 // Otherwise, use the webservice driver
 $driver = new UNL_Peoplefinder_Driver_WebService(array('service_url'=>'http://ucommbieber.unl.edu/workspace/peoplefinder/www/service.php'));
 
 /*
-//database connection info
+//database connection info for officefinder portions
 UNL_Officefinder::$db_user = '';
 UNL_Officefinder::$db_pass = '';
+
+// Officefinder editing admins
 UNL_Officefinder::$admins = array('bbieber2', 'smeranda', 'erasmussen2');
 */
