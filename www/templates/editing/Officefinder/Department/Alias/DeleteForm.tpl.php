75955fc22e819cdfd1028639ca8721bfab433b6e
Brett Bieber <brett.bieber@gmail.com>
2010-09-20 19:37:11 +0000

    Add some consistency to the delete form markup. Add class of delete on the form.

--- www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
@@ -1,5 +1,5 @@
-<a href="#" onclick="if (confirm('Are you sure? This will delete the alias.')) document.getElementById('deletealias_<?php echo $context->department_id.$context->name; ?>').submit(); else return false;">Delete</a>
-<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deletealias_<?php echo $context->department_id.$context->name; ?>" style="width:120px;">
+<a href="#" class="action delete" onclick="if (confirm('Are you sure? This will delete the alias.')) document.getElementById('deletealias_<?php echo $context->department_id.$context->name; ?>').submit(); else return false;">Delete</a>
+<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deletealias_<?php echo $context->department_id.$context->name; ?>" style="width:120px;" class="delete">
     <input type="hidden" name="_type" value="delete_dept_alias" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
     <input type="hidden" name="name" value="<?php echo $context->name; ?>" />
--- www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php
@@ -1,5 +1,5 @@
-<a href="#" onclick="if (confirm('Are you sure? This will remove editing access for this user.')) document.getElementById('deleteuser_<?php echo $context->department_id.$context->uid; ?>').submit(); else return false;">Delete</a>
-<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deleteuser_<?php echo $context->department_id.$context->uid; ?>" style="width:120px;">
+<a href="#" class="action delete" onclick="if (confirm('Are you sure? This will remove editing access for this user.')) document.getElementById('deleteuser_<?php echo $context->department_id.$context->uid; ?>').submit(); else return false;">Delete</a>
+<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deleteuser_<?php echo $context->department_id.$context->uid; ?>" style="width:120px;" class="delete">
     <input type="hidden" name="_type" value="delete_dept_user" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
     <input type="hidden" name="uid" value="<?php echo $context->uid; ?>" />
