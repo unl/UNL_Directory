48cf8dedb937492d0bc83145cb41a70bcaeb07a7
Brett Bieber <brett.bieber@gmail.com>
2010-09-21 17:00:53 +0000

    Make sure the submit button text is white.

--- www/css/zenform_anywhere.css.php
+++ www/css/zenform_anywhere.css.php
@@ -3,6 +3,10 @@ $zenform = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/wdn/templates_3.0/css/c
 header('Content-type: text/css');
 echo str_replace('#maincontent ', '', $zenform);
 ?>
+
 h3.zenform {
+    color:#fff !important;
+}
+form.zenform input[type="submit"] {
     color:#fff !important;
 }
