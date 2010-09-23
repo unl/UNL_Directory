f10eaa9e0818be006c33a853750218e52316ee13
Seth Meranda <smeranda2@unl.edu>
2010-09-23 21:08:40 +0000

    wording update.

--- www/templates/html/Officefinder/Department/Users.tpl.php
+++ www/templates/html/Officefinder/Department/Users.tpl.php
@@ -1,5 +1,5 @@
 <?php
-echo '<h5><a href="#" class="tooltip" title="Give others permission to edit this entry, and ALL children">Users</a></h5>';
+echo '<h5><a href="#" class="tooltip" title="Give others permission to edit this entry and ALL children.">Users</a></h5>';
 if (count($context)) {
     echo '<ul class="dept_users">';
     foreach ($context as $user) {
