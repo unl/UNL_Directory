e05f15852a6f9e1a568fefafbd666c47d34cb9f2
Brett Bieber <brett.bieber@gmail.com>
2010-09-30 19:17:11 +0000

    Add list of all users.

---
+++ src/UNL/Officefinder/Users.php
@@ -0,0 +1,18 @@
+<?php
+class UNL_Officefinder_Users extends UNL_Officefinder_UserList
+{
+    function __construct($options = array())
+    {
+
+        $users = array();
+        $mysqli = UNL_Officefinder::getDB();
+        $sql = 'SELECT DISTINCT uid FROM department_permissions';
+        if ($result = $mysqli->query($sql)) {
+            while($row = $result->fetch_array(MYSQLI_NUM)) {
+                $users[] = $row[0];
+            }
+        }
+        $mysqli->close();
+        parent::__construct($users);
+    }
+}
