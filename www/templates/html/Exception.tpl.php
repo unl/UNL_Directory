3fbc755c174c8ff4cb6f2adc56a10101af1c0963
Brett Bieber <brett.bieber@gmail.com>
2010-10-01 03:17:07 +0000

    Add sexy error messages.

--- www/templates/html/Exception.tpl.php
+++ www/templates/html/Exception.tpl.php
@@ -4,4 +4,17 @@ if (false == headers_sent()
     header('HTTP/1.1 '.$code.' '.$context->getMessage());
     header('Status: '.$code.' '.$context->getMessage());
 }
-echo $context->getMessage();
+?>
+
+<script type="text/javascript">
+WDN.initializePlugin('notice');
+</script>
+<div class="wdn_notice alert">
+    <div class="close">
+        <a href="#" title="Close this notice">Close this notice</a>
+    </div>
+    <div class="message">
+        <h4>Whoops! Sorry, there was an error:</h4>
+        <p><?php echo $context->getMessage(); ?></p>
+    </div>
+</div>
