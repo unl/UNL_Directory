0fc74fdb2e81db67b4363980c6731231ec99a549
Brett Bieber <brett.bieber@gmail.com>
2010-10-01 20:30:29 +0000

    Improve listing details:

--- www/templates/html/Officefinder/Department/Listing.tpl.php
+++ www/templates/html/Officefinder/Department/Listing.tpl.php
@@ -4,18 +4,22 @@ if (preg_match('/^([A-Z]+)\s/', $context->address, $matches)) {
     $address = str_replace($matches[1], '<a class="location mapurl" href="http://maps.unl.edu/#'.$matches[1].'">'.$matches[1].'</a>', $context->address);
 }
 
-echo '<div class="edit">';
 if ($context->userCanEdit(UNL_Officefinder::getUser())) {
+    echo '<div class="edit">';
     echo ' <a href="'.$context->getURL().'&amp;format=editing" class="action edit" title="Edit">edit</a>'.PHP_EOL;
     echo $savvy->render($context, 'Officefinder/Department/Listing/SortForm.tpl.php');
     include dirname(__FILE__).'/../../../editing/Officefinder/Department/DeleteForm.tpl.php';
     echo ' <a href="'.UNL_Officefinder::getURL(null, array('view'      => 'department',
                                                           'parent_id' => $context->id)).'&amp;format=editing" class="action addchild" title="Add Child">Add child</a>';
+    echo '</div>';
 }
-echo '</div>';
-
-echo '<div class="listingDetails">';
-echo $context->name.' <span class="tel">'.$savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php').'</span>';
-echo ' <span class="adr">'.$address.'</span>';
-echo '</div>';
 ?>
+<div class="listingDetails">
+    <?php echo $context->name ?>
+    <span class="tel"><?php echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php') ?></span>
+    <?php if (isset($context->building)): ?>
+    <span class="room"><?php echo $context->room.' <a class="location mapurl" href="http://maps.unl.edu/#'.$context->building.'">'.$context->building.'</a>'; ?></span>
+    <?php endif; ?>
+    <span class="adr"><?php echo $address; ?></span>
+    <span class="postal-code"><?php echo $context->postal_code; ?></span>
+</div>
