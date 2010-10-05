071b8e8868af591fe87bdb312d9f4b96b4528b9a
Seth Meranda <smeranda2@unl.edu>
2010-10-05 20:58:44 +0000

    put the search notice after the new directory help and clear the floats.

--- www/scripts/peoplefinder.js
+++ www/scripts/peoplefinder.js
@@ -184,7 +184,7 @@ var directory = function() {
 		
 		buildSearchNotice : function(originalSearch, firstName, lastName) {
 			WDN.jQuery("#searchNotice").remove();
-			WDN.jQuery("#peoplefinder").after('<p id="searchNotice">Your original search for <span>'+originalSearch+'</span> did not return any results. So we tried a few more advanced searches and below is what we found for <span>First Name: '+firstName+' AND Last Name: '+lastName+'</span>.');
+			WDN.jQuery("#directoryHelp").after('<p id="searchNotice">Your original search for <span>'+originalSearch+'</span> did not return any results. So we tried a few more advanced searches and below is what we found for <span>First Name: '+firstName+' AND Last Name: '+lastName+'</span>.');
 		},
 		
 		showSearchNotice : function() {
