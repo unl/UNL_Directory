cd1e220e9cb29c765804fd9a2ca5cb94b0b7cf11
Seth Meranda <smeranda2@unl.edu>
2010-10-07 15:20:34 +0000

    put a max-width on the aliases names so the delete icon stays on the same level

--- www/templates/html/Officefinder/Department/Alias.tpl.php
+++ www/templates/html/Officefinder/Department/Alias.tpl.php
@@ -1,3 +1,3 @@
-<li><?php echo $context; ?>
+<li><span><?php echo $context; ?></span>
  <?php include dirname(UNL_Peoplefinder::getDataDir()).'/www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php'; ?>
 </li>
