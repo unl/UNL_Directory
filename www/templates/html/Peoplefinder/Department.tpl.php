c47c213c02e5d5bca2499fc5c03d9ba9f9876a57
Brett Bieber <brett.bieber@gmail.com>
2010-10-27 15:01:17 +0000

    Hard-code to not display student affilations when displaying departments.

--- www/templates/html/Peoplefinder/Department.tpl.php
+++ www/templates/html/Peoplefinder/Department.tpl.php
@@ -22,6 +22,11 @@ if (count($context)) {
     echo '<div id="all_employees">';
     $by_affiliation = UNL_Peoplefinder_SearchResults::groupByAffiliation($context->getRawObject());
 
+    // Hide various affilations
+    if (isset($by_affiliation['student'])) {
+        unset($by_affiliation['student']);
+    }
+
     ksort($by_affiliation);
     foreach ($by_affiliation as $affiliation=>$records) {
         if (count($records)) {
