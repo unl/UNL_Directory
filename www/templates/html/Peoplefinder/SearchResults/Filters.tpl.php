10ab057d8d93f5e900dd3ea0d1d8534e9a69d297
Brett Bieber <brett.bieber@gmail.com>
2010-10-29 14:04:34 +0000

    Change remove "People" from filter heading, since we'll be showing both people and departments.

--- www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
@@ -1,6 +1,6 @@
 <div id="filters" class="one_col left">
     <div class="zenbox energetic wdn_filterset">
-        <h3>Filter People Results</h3>
+        <h3>Filter Results</h3>
         <form method="post" action="#" class="filters">
             <fieldset class="affiliation">
                 <legend><span>By Affiliation</span></legend>
