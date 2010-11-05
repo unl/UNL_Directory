29bcbce1a9d48425e240100e073c042db741c1dd
Brett Bieber <brett.bieber@gmail.com>
2010-11-05 21:47:53 +0000

    Add link to the printer-friendly version of the site.

--- www/templates/html/Peoplefinder/Instructions.tpl.php
+++ www/templates/html/Peoplefinder/Instructions.tpl.php
@@ -38,6 +38,7 @@
 	        	<li>Complete list of department employees</li>
 	        	<li>Organizational heirarchy of department</li>
 	        </ul>
+	        <p>A printer friendly view is also available for UNL users. <a href="<?php echo UNL_Officefinder::getURL(); ?>?view=alphalisting">Log in and view the printer-friendly Yellow Page Directory</a></p>
 	    </div>
     </div>
 </div>
