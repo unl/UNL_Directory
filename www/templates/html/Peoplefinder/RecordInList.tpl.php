c98a25d6e7db27431341caf65c16b0003ecfe635
Brett Bieber <brett.bieber@gmail.com>
2010-11-05 16:07:01 +0000

    Pass the options through. Make sure "choose this person" works correctly.

--- www/templates/html/Peoplefinder/RecordInList.tpl.php
+++ www/templates/html/Peoplefinder/RecordInList.tpl.php
@@ -41,7 +41,7 @@ if (isset($context->telephoneNumber)) {
 
 echo '    </div>'.PHP_EOL;
 echo '    <a href="'.UNL_Peoplefinder::getURL().'?uid='.$context->uid.'" class="cInfo" '.$onclick.'>Contact '.$context->givenName.'</a><div class="loading"></div>'.PHP_EOL;
-if (isset($parent->context->options['chooser'])) {
+if (isset($parent->parent->context->options['chooser'])) {
     echo '    <div class="pfchooser"><a href="#" onclick="return pfCatchUID(\''.$context->uid.'\');">Choose this person</a></div>'.PHP_EOL;
 }
 echo '</div></li>';
--- www/templates/html/Peoplefinder/SearchResults/ByAffiliation.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults/ByAffiliation.tpl.php
@@ -1,11 +1,11 @@
 <?php
 echo '<div id="results_'.$context->affiliation.'" class="results affiliation '.$context->affiliation.'">';
 echo '<h2>'.ucfirst($context->affiliation).'</h2>';
-echo $savvy->render(new UNL_Peoplefinder_SearchResults(array('results'=>$context->results)));
+echo $savvy->render(new UNL_Peoplefinder_SearchResults(array('results'=>$context->results, 'options'=>$context->options )));
 if (count($context->like_results)) {
     echo '<div class="likeResults">';
     echo '<h3>similar '.$context->affiliation.' results</h3>';
-    echo $savvy->render(new UNL_Peoplefinder_SearchResults(array('results'=>$context->like_results)));
+    echo $savvy->render(new UNL_Peoplefinder_SearchResults(array('results'=>$context->like_results, 'options'=>$context->options)));
     echo '</div>';
 }
 echo '</div>';
