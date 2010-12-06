a9082d85353395a975d793719016785227004ca7
Brett Bieber <brett.bieber@gmail.com>
2010-12-06 15:16:39 +0000

    Link the users for departments so you can access their records. Make delete/action link selector more specific so it doesn't affect the uid link.

--- www/templates/html/Officefinder/Department/User.tpl.php
+++ www/templates/html/Officefinder/Department/User.tpl.php
@@ -1,3 +1,3 @@
-<li><?php echo $context->uid; ?>
+<li><a href="<?php echo UNL_Peoplefinder::getURL().'?uid='.$context->uid; ?>"><?php echo $context->uid; ?></a>
  <?php include dirname(UNL_Peoplefinder::getDataDir()).'/www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php'; ?>
 </li>
