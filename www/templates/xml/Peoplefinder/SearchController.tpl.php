58ecd167f1ad77674f89cc8835374784f852069c
Brett Bieber <brett.bieber@gmail.com>
2010-12-15 15:16:18 +0000

    Allow "like" results to be returned for the XML output.

--- www/templates/xml/Peoplefinder/SearchController.tpl.php
+++ www/templates/xml/Peoplefinder/SearchController.tpl.php
@@ -1,2 +1,14 @@
 <?php
+
+$like_records = array();
+if (!is_array($context->options['q'])
+    && UNL_Peoplefinder::$displayResultLimit) {
+    // More room to display like results
+    $like_records = $context->options['peoplefinder']->getLikeMatches($context->options['q'], null, $context->results);
+}
+
 echo $savvy->render($context->results);
+
+if ($like_records) {
+    echo $savvy->render($like_records);
+}
