898508cf746215f61e74c4b0240f43fb519ccf43
Brett Bieber <brett.bieber@gmail.com>
2010-12-15 15:27:34 +0000

    Decrement the displayresultlimit, and check if a method was specified. If a specific method was passed, do not add in like results.

--- www/templates/xml/Peoplefinder/SearchController.tpl.php
+++ www/templates/xml/Peoplefinder/SearchController.tpl.php
@@ -1,7 +1,10 @@
 <?php
 
+UNL_Peoplefinder::$displayResultLimit -= count($context->results);
+
 $like_records = array();
 if (!is_array($context->options['q'])
+    && !isset($context->options['method'])
     && UNL_Peoplefinder::$displayResultLimit) {
     // More room to display like results
     $like_records = $context->options['peoplefinder']->getLikeMatches($context->options['q'], null, $context->results);
