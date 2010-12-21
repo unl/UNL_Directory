4d2644765ecf7d3dbaac68e1a5c665e945dfa850
Brett Bieber <brett.bieber@gmail.com>
2010-12-21 16:54:16 +0000

    Fix syntax error typo

--- src/UNL/Peoplefinder/Driver/WebService.php
+++ src/UNL/Peoplefinder/Driver/WebService.php
@@ -119,7 +119,7 @@ class UNL_Peoplefinder_Driver_WebService implements UNL_Peoplefinder_DriverInter
     
     public function getHROrgUnitNumberMatches($query, $affiliation = null)
     {
-        $results = file_get_contents('http://directory.unl.edu/departments/?view=deptlistings&org_unit='.urlencode($query).'&format=php'');
+        $results = file_get_contents('http://directory.unl.edu/departments/?view=deptlistings&org_unit='.urlencode($query).'&format=php');
         if ($results) {
             $results = unserialize($results);
         }
