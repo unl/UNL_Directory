e594194a26b065ca3f549c1e9c2cfb0bad3fe7b1
Brett Bieber <brett.bieber@gmail.com>
2011-01-04 23:44:09 +0000

    Improve the department name search query by using JOINs

--- src/UNL/Officefinder/DepartmentList/NameSearch.php
+++ src/UNL/Officefinder/DepartmentList/NameSearch.php
@@ -8,15 +8,21 @@ class UNL_Officefinder_DepartmentList_NameSearch extends UNL_Officefinder_Depart
         $this->options = $options + $this->options;
         $records = array();
         $mysqli = UNL_Officefinder::getDB();
-        $sql = 'SELECT DISTINCT departments.id FROM departments, department_aliases
-                WHERE (departments.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%" OR
-                    (department_aliases.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%" AND department_aliases.department_id = departments.id))';
+        $sql = 'SELECT DISTINCT d1.id
+                FROM departments d1 ';
         if ((bool)$this->options['parent_orgs'] === true) {
             // Preorder Tree model
             // $sql .= ' AND (departments.rgt != departments.lft+1 OR departments.org_unit IS NOT NULL) ';
-            $sql .= ' AND departments.id IN (SELECT DISTINCT departments.parent_id FROM departments WHERE parent_id IS NOT NULL) ';
+            $sql .= 'INNER JOIN departments d2 ON d2.parent_id = d1.id ';
         }
-        $sql .= ' ORDER BY departments.name';
+        $sql .= '
+                LEFT JOIN department_aliases ds ON ds.department_id = d1.id
+                WHERE (
+                d1.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%"
+                OR
+                ds.name LIKE "%'.$mysqli->escape_string($this->options['q']).'%"
+                )
+                ORDER BY d1.name';
         if ($result = $mysqli->query($sql)) {
             while($row = $result->fetch_array(MYSQLI_NUM)) {
                 $records[] = $row[0];
