fa7a2fa614a8781bc0f7818c80bb25cd4b6291c6
Seth Meranda <smeranda2@unl.edu>
2011-01-11 21:13:21 +0000

    style the multiple major listings.

--- www/templates/html/Peoplefinder/Record.tpl.php
+++ www/templates/html/Peoplefinder/Record.tpl.php
@@ -54,9 +54,9 @@ if (isset($context->unlSISClassLevel)) {
         default:
             $class = $context->unlSISClassLevel;
     }
-    echo '<span class="title"><span class="grade">'.$class.'</span>, ';
+    echo '<span class="title"><span class="grade">'.$class.'</span>';
     foreach ($context->unlSISMajor as $major) {
-        echo '<span class="major">'.$context->formatMajor($major).'</span> <br />';// <span class="college">'.$context->formatCollege((string) $context->unlSISCollege).'</span>';
+        echo '<span class="major">'.$context->formatMajor($major).'</span>';// <span class="college">'.$context->formatCollege((string) $context->unlSISCollege).'</span>';
     }
     echo '</span>';
 }
