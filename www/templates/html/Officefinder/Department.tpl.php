67d283454fba3e5b804623d81624a63cb33d4603
Michael Fairchild <mfairchild365@gmail.com>
2011-01-12 22:07:17 +0000

    Added name and email fields to correction forms.

--- www/templates/html/Officefinder/Department.tpl.php
+++ www/templates/html/Officefinder/Department.tpl.php
@@ -167,6 +167,8 @@ if (!$context->isRoot()) {
 	<h3>Have a correction?</h3>
 	<form class="wdn_feedback_comments2" method="post" action="http://www1.unl.edu/comments/">
 		<input type="hidden" id="page_address" value="" />
+		Name: <input type="text" name="name" id="name" value="" />
+        Email: <input type="text" name="email" id="email" value="" />
 		<textarea name="comment" id="comment" rows="" cols=""></textarea>
 		<input type="submit" value="Submit" />
 	</form>
