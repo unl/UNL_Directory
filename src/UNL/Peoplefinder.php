dc9f69a57f4d942a961f1b36ab0c572d6156db27
Brett Bieber <brett.bieber@gmail.com>
2011-01-13 15:17:00 +0000

    Improve readability of the mobile client check. Add check for wdn_mobile=no cookie.

--- src/UNL/Peoplefinder.php
+++ src/UNL/Peoplefinder.php
@@ -99,17 +99,9 @@ class UNL_Peoplefinder
 
         $this->options = $options + $this->options;
 
-        if (isset($_SERVER['HTTP_ACCEPT'], $_SERVER['HTTP_USER_AGENT'])
-            && $this->options['format'] == 'html' && (
-                ($this->options['mobile'] !== false && $this->options['mobile'] != 'no')
-                || (preg_match('/text\/vnd\.wap\.wml|application\/vnd\.wap\.xhtml\+xml/', $_SERVER['HTTP_ACCEPT']))
-                    || preg_match('/sony|symbian|nokia|samsung|mobile|windows ce|epoc|opera mini/', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/nitro|j2me|midp-|cldc-|netfront|mot|up\.browser|up\.link|audiovox/', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/blackberry|ericsson,|panasonic|philips|sanyo|sharp|sie-/', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/portalmmm|blazer|avantgo|danger|palm|series60|palmsource|pocketpc/', $_SERVER['HTTP_USER_AGENT'])
-                    || preg_match('/smartphone|rover|ipaq|au-mic,|alcatel|ericy|vodafone\/|wap1\.|wap2\.|iPhone|Android/', $_SERVER['HTTP_USER_AGENT'])
-            )) {
-            $this->options['mobile'] = true;
+        if ($this->options['format'] == 'html'
+            && $this->options['mobile'] != 'no') {
+            $this->options['mobile'] = self::isMobileClient();
         }
 
         try {
@@ -117,6 +109,35 @@ class UNL_Peoplefinder
         } catch(Exception $e) {
             $this->output[] = $e;
         }
+    }
+
+    public static function isMobileClient($options = array())
+    {
+        if (!isset($_SERVER['HTTP_ACCEPT'], $_SERVER['HTTP_USER_AGENT'])) {
+            // We have no vars to check
+            return false;
+        }
+
+        if (isset($_COOKIE['wdn_mobile'])
+            && $_COOKIE['wdn_mobile'] == 'no') {
+            // The user has a cookie set, requesting no mobile views
+            return false;
+        }
+
+        if ( // Check the http_accept and user agent and see
+            preg_match('/'.
+               'text\/vnd\.wap\.wml|application\/vnd\.wap\.xhtml\+xml|' .
+               'sony|symbian|nokia|samsung|mobile|windows ce|epoc|opera mini|' .
+               'nitro|j2me|midp-|cldc-|netfront|mot|up\.browser|up\.link|audiovox|' .
+               'blackberry|ericsson,|panasonic|philips|sanyo|sharp|sie-|' .
+               'portalmmm|blazer|avantgo|danger|palm|series60|palmsource|pocketpc|' .
+               'smartphone|rover|ipaq|au-mic,|alcatel|ericy|vodafone\/|wap1\.|wap2\.|iPhone|Android' .
+               '/', $_SERVER['HTTP_USER_AGENT'])
+           ) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
