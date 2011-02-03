c5ddd3fa3fadc8f3ce5f636f66f2d4e73ad0dad2
Seth Meranda <smeranda2@unl.edu>
2011-02-03 17:27:12 +0000

    keep the ipad out of the mobile view.

--- src/UNL/Peoplefinder.php
+++ src/UNL/Peoplefinder.php
@@ -127,14 +127,14 @@ class UNL_Peoplefinder
         if ( // Check the http_accept and user agent and see
             preg_match('/text\/vnd\.wap\.wml|application\/vnd\.wap\.xhtml\+xml/i', $_SERVER['HTTP_ACCEPT'])
                 || 
-            preg_match('/'.
+            (preg_match('/'.
                'sony|symbian|nokia|samsung|mobile|windows ce|epoc|opera mini|' .
                'nitro|j2me|midp-|cldc-|netfront|mot|up\.browser|up\.link|audiovox|' .
                'blackberry|ericsson,|panasonic|philips|sanyo|sharp|sie-|' .
                'portalmmm|blazer|avantgo|danger|palm|series60|palmsource|pocketpc|' .
                'smartphone|rover|ipaq|au-mic|alcatel|ericy|vodafone\/|wap1\.|wap2\.|iPhone|Android' .
                '/i', $_SERVER['HTTP_USER_AGENT'])
-           ) {
+           ) && !preg_match('/ipad/i', $_SERVER['HTTP_USER_AGENT'])) {
             return true;
         }
 
