61d571636a5233ebb7a7e3265a4fb941c4431e49
Seth Meranda <smeranda2@unl.edu>
2011-02-16 23:07:59 +0000

    only echo results if they exist.

--- www/templates/json/Peoplefinder/SearchController.tpl.php
+++ www/templates/json/Peoplefinder/SearchController.tpl.php
@@ -9,8 +9,9 @@ if (!is_array($context->options['q'])
     // More room to display like results
     $like_records = $context->options['peoplefinder']->getLikeMatches($context->options['q'], null, $context->results);
 }
-
-echo $savvy->render($context->results);
+if (count($context->results)){
+	echo $savvy->render($context->results);
+}
 
 if ($like_records) {
     echo $savvy->render($like_records);
