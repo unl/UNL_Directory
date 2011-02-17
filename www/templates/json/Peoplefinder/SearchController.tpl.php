f8db5a144bba417b7b9492f9902960b2201ef923
Brett Bieber <brett.bieber@gmail.com>
2011-02-17 14:56:11 +0000

    Loop through all results and render them. Once we have all the individual records, implode them and echo as a json array.

--- www/templates/json/Peoplefinder/SearchController.tpl.php
+++ www/templates/json/Peoplefinder/SearchController.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 UNL_Peoplefinder::$displayResultLimit -= count($context->results);
 
 $like_records = array();
@@ -7,12 +6,21 @@ if (!is_array($context->options['q'])
     && !isset($context->options['method'])
     && UNL_Peoplefinder::$displayResultLimit) {
     // More room to display like results
+        
     $like_records = $context->options['peoplefinder']->getLikeMatches($context->options['q'], null, $context->results);
 }
-if (count($context->results)){
-	echo $savvy->render($context->results);
+
+$all_results = array();
+
+foreach ($context->results as $result) {
+    $all_results[] = $savvy->render($result);
 }
 
-if ($like_records) {
-    echo $savvy->render($like_records);
+
+foreach ($like_records as $result) {
+    $all_results[] = $savvy->render($result);
 }
+
+echo '['.implode(',', $all_results).']';
+
+?>
