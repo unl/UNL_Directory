012795352139a1e243be3eae7b54982802954238
Brett Bieber <brett.bieber@gmail.com>
2011-04-20 22:13:10 +0000

    If the org unit is OOTP, set it as so.

--- www/templates/html/Peoplefinder/Person/Roles.tpl.php
+++ www/templates/html/Peoplefinder/Person/Roles.tpl.php
@@ -5,9 +5,9 @@ foreach ($context as $role) {
         continue;
     }
     $parent_name = 'University of Nebraska&ndash;Lincoln';
-//    if ($context->unlHRPrimaryDepartment == 'Office of the President') {
-//        $parent_name = 'University of Nebraska';
-//    }
+    if ($org->org_unit == '50000094') {
+        $parent_name = 'University of Nebraska';
+    }
     $dept_url = $org->getURL();
     echo "<span class='org'><span class='title'>{$role->description}</span>\n\t<span class='organization-unit'><a href='{$dept_url}'>{$org->name}</a></span>\n\t<span class='organization-name'>$parent_name</span></span>\n";
 }
