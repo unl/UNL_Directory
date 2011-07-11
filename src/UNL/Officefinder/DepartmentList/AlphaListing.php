dcc530b1c98611c6816b76bcb664da2f20ca2337
Brett Bieber <brett.bieber@gmail.com>
2011-07-11 13:41:04 +0000

    Refactor so the constructor isn't so dang huge

--- src/UNL/Officefinder/DepartmentList/AlphaListing.php
+++ src/UNL/Officefinder/DepartmentList/AlphaListing.php
@@ -10,23 +10,34 @@ class UNL_Officefinder_DepartmentList_AlphaListing extends FilterIterator implem
         // Require login to view the full directory
         UNL_Officefinder::getUser(true);
 
+        parent::__construct($this->getIterator());
+    }
+
+    protected function getIterator()
+    {
         $records = array();
         $mysqli = UNL_Officefinder::getDB();
-        $sql = 'SELECT DISTINCT departments.id AS id, departments.name AS name
-                FROM departments
-                WHERE org_unit IS NOT NULL
-                    UNION
-                SELECT department_aliases.department_id as id, CONCAT(department_aliases.name, " (see ", departments.name, ")") as name
-                FROM department_aliases, departments
-                WHERE department_aliases.department_id = departments.id
-
-                ORDER BY name;';
+        $sql = $this->getSQL();
         if ($result = $mysqli->query($sql)) {
             while($row = $result->fetch_array(MYSQLI_NUM)) {
                 $records[] = $row;
             }
         }
-        parent::__construct(new ArrayIterator($records));
+        return new ArrayIterator($records);
+    }
+
+    protected function getSQL()
+    {
+        $sql = 'SELECT DISTINCT departments.id AS id, departments.name AS name
+                        FROM departments
+                        WHERE org_unit IS NOT NULL
+                            UNION
+                        SELECT department_aliases.department_id as id, CONCAT(department_aliases.name, " (see ", departments.name, ")") as name
+                        FROM department_aliases, departments
+                        WHERE department_aliases.department_id = departments.id
+        
+                        ORDER BY name;';
+        return $sql;
     }
 
     function preRun($cached)
