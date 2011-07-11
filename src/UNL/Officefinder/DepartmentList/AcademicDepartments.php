cc22630001fd836b56330eef868874fc06c74f67
Brett Bieber <brett.bieber@gmail.com>
2011-07-11 13:48:12 +0000

    No need to override constructor now

---
+++ src/UNL/Officefinder/DepartmentList/AcademicDepartments.php
@@ -0,0 +1,18 @@
+<?php
+class UNL_Officefinder_DepartmentList_AcademicDepartments extends UNL_Officefinder_DepartmentList_AlphaListing
+{
+
+    protected function getSQL()
+    {
+        $sql = 'SELECT DISTINCT departments.id AS id, departments.name AS name
+                FROM departments
+                WHERE org_unit IS NOT NULL AND academic = 1
+                    UNION
+                SELECT department_aliases.department_id as id, CONCAT(department_aliases.name, " (see ", departments.name, ")") as name
+                FROM department_aliases, departments
+                WHERE department_aliases.department_id = departments.id
+
+                ORDER BY name;';
+        return $sql;
+    }
+}
