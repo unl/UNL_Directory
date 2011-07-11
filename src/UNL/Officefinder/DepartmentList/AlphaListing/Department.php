393ae72e976575c031b5eb5dc613a36f9d77c889
Brett Bieber <brett.bieber@gmail.com>
2011-07-11 15:59:34 +0000

    Pass through requests for variables and methods

--- src/UNL/Officefinder/DepartmentList/AlphaListing/Department.php
+++ src/UNL/Officefinder/DepartmentList/AlphaListing/Department.php
@@ -13,4 +13,14 @@ class UNL_Officefinder_DepartmentList_AlphaListing_Department
         $this->department = $department;
         $this->options    = $options;
     }
+
+    function __call($method, $params)
+    {
+        return call_user_func_array(array($this->department, $method), $params);
+    }
+
+    function __get($var)
+    {
+        return $this->department->$var;
+    }
 }
