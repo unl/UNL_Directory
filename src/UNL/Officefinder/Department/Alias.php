69470fa544e0540e6df855e2893c0f9e6818434e
Brett Bieber <brett.bieber@gmail.com>
2011-10-31 15:30:12 +0000

    Only allow admins to add aliases which already return the department

--- src/UNL/Officefinder/Department/Alias.php
+++ src/UNL/Officefinder/Department/Alias.php
@@ -27,6 +27,23 @@ class UNL_Officefinder_Department_Alias extends UNL_Officefinder_Record
         return false;
     }
 
+    function insert()
+    {
+        // Check if we can already match based on this name
+        if ($departments = new UNL_Officefinder_DepartmentList_NameSearch(array('q'=>$this->name))) {
+            if (in_array($this->department_id, $departments->getArrayCopy())
+                && !UNL_Officefinder::isAdmin(UNL_Officefinder::getUser())) {
+                // This search alias already matches the department
+                throw new Exception(
+                    'That alias will already return your department.
+                    If you must have this alias added, contact a directory administrator.
+                    Here is a list of directory admins — '.implode(', ', UNL_Officefinder::$admins), 500);
+            }
+        }
+
+        return parent::insert();
+    }
+
     function __toString()
     {
         return $this->name;
