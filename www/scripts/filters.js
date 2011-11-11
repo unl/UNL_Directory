892e9960edeec5f524e64f09bd4fcef4dfada77b
Seth Meranda <smeranda2@unl.edu>
2011-11-11 03:10:35 +0000

    Remove link function for filter summary items. Fixes [#963]

--- www/scripts/filters.js
+++ www/scripts/filters.js
@@ -80,15 +80,16 @@ var filters = function() {
 		updateSummary: function(ids) { //this function recives an array of all checked filters
 			if (ids.length < 1) { //nothing in the array, therefore it's ALL
 				WDN.jQuery('#filterSummary a, span.operator').remove();
-				WDN.jQuery('#filterSummary').append('<a href="" class="all">All Options</a>');
+				WDN.jQuery('#filterSummary').append('<a href="#" class="all">All Options</a>');
 				WDN.jQuery('.filterAll').attr('checked', 'checked');
 				filters.showAll();
 			} else { //at least one id exists in the array
 				WDN.jQuery('#filterSummary a, span.operator').remove();
 				WDN.jQuery.each(ids, function(key, value){
-					WDN.jQuery('#filterSummary').append(' <a href="" class="'+WDN.jQuery('#'+value).attr('value')+'"><span class="group">'+WDN.jQuery('#'+value).closest('fieldset').children('legend').text()+':</span> '+WDN.jQuery('#'+value).siblings('label').text()+'</a><span class="operator"> OR </span>');
+					WDN.jQuery('#filterSummary').append(' <a href="#" class="'+WDN.jQuery('#'+value).attr('value')+'"><span class="group">'+WDN.jQuery('#'+value).closest('fieldset').children('legend').text()+':</span> '+WDN.jQuery('#'+value).siblings('label').text()+'</a><span class="operator"> OR </span>');
 				});
 			}
+			WDN.jQuery('#filterSummary a').live('click', function(e){e.preventDefault;return false;});
 			
 		},
 		
