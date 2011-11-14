bd214edd8cc88d9d42834e95f57c6ca0815c0e47
Seth Meranda <smeranda2@unl.edu>
2011-11-14 19:11:13 +0000

    change text to something more universal.

--- www/templates/editing/Officefinder/Department/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/DeleteForm.tpl.php
@@ -1,4 +1,4 @@
-<a href="#" class="delete warning" title="Delete" onclick="if (confirm('Are you sure? This will delete this record and <?php echo count($context->getChildren()); ?> children.')) document.getElementById('deletedepartment_<?php echo $context->id; ?>').submit(); else return false;">Delete this department</a>
+<a href="#" class="delete warning" title="Delete" onclick="if (confirm('Are you sure? This will delete this record and <?php echo count($context->getChildren()); ?> children.')) document.getElementById('deletedepartment_<?php echo $context->id; ?>').submit(); else return false;">Delete this listing</a>
 <form action="<?php echo $context->getURL(); ?>" method="post" id="deletedepartment_<?php echo $context->id; ?>" class="delete">
     <input type="hidden" name="_type" value="delete_department" />
     <input type="hidden" name="department_id" value="<?php echo $context->id; ?>" />
