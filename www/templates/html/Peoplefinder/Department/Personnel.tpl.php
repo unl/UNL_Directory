bca88aa0abe7f6d144cd65940e4f5870614236e7
Brett Bieber <brett.bieber@gmail.com>
2011-11-15 20:34:49 +0000

    Do not display department personnel by affiliation
    When displaying department personnel, we do not know if the affiliation applies to the current department, so it is best to not group personnel by affiliation.

--- www/templates/html/Peoplefinder/Department/Personnel.tpl.php
+++ www/templates/html/Peoplefinder/Department/Personnel.tpl.php
@@ -1,21 +1,5 @@
+<div id="all_employees">
 <?php
-echo '<div id="all_employees">';
-$by_affiliation = UNL_Peoplefinder_SearchResults::groupByAffiliation($context->getRawObject());
-
-// Hide various affilations
-if (isset($by_affiliation['student'])) {
-    unset($by_affiliation['student']);
-}
-
-ksort($by_affiliation);
-foreach ($by_affiliation as $affiliation=>$records) {
-    if (count($records)) {
-        $section               = new stdClass();
-        $section->affiliation  = $affiliation;
-        $section->results      = $records;
-        $section->like_results = array();
-        $section->options      = $context->options;
-        echo $savvy->render($section, 'Peoplefinder/SearchResults/ByAffiliation.tpl.php');
-    }
-}
-echo '</div>';
+echo $savvy->render($context, 'Peoplefinder/SearchResults.tpl.php');
+?>
+</div>
