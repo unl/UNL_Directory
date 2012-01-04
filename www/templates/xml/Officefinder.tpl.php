7f4b594e3081cd94cf3c9db50c98c52435eab431
Brett Bieber <brett.bieber@gmail.com>
2012-01-04 16:45:44 +0000

    Add XML output format for department information

---
+++ www/templates/xml/Officefinder.tpl.php
@@ -0,0 +1,2 @@
+<?php
+echo $savvy->render($context, 'Peoplefinder.tpl.php');
---
+++ www/templates/xml/Officefinder/Department.tpl.php
@@ -0,0 +1,15 @@
+<department id="<?php echo $context->id; ?>">
+    <?php if (!$context->isRoot()): ?>
+    <parent xlink:href="<?php echo UNL_Officefinder::getURL().$context->getParent()->id; ?>?format=xml" />
+    <?php endif; ?>
+    <?php foreach ($context as $key=>$value):
+    if (is_array($value)) {
+        continue;
+    }
+    ?>
+    <?php echo "<$key>$value</$key>\n"; ?>
+    <?php endforeach; ?>
+<?php foreach ($context->getChildren() as $child): ?>
+    <child xlink:href="<?php echo UNL_Officefinder::getURL().$child->id; ?>?format=xml" />
+<?php endforeach; ?>
+</department>
