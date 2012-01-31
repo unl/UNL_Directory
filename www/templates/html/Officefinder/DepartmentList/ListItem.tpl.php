a42f0c42400665f1466aebcae68ce0c19d4acbc2
Kevin Abel <kabel2@unl.edu>
2012-01-31 21:55:19 +0000

    Remove a double quote and use inline markup.

--- www/templates/html/Officefinder/DepartmentList/ListItem.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/ListItem.tpl.php
@@ -10,20 +10,21 @@ $li_class = 'dep_result parent_'.$context->id;
 if ($parent) {
     $li_class .= ' parent_'.$parent->id;
 }
-echo '<li class="'.$li_class.'">
+?>
+<li class="<?php echo $li_class ?>">
     <div class="overflow">
-    <a class="planetred_profile" href="'.$context->getURL().'">
-    <img alt="Generic Icon" src="'.UNL_Peoplefinder::getURL().'images/organization40.png" class="profile_pic small photo">
+    <a class="planetred_profile" href="<?php echo $context->getURL() ?>">
+    <img alt="Generic Icon" src="<?php echo UNL_Peoplefinder::getURL() ?>images/organization40.png" class="profile_pic small photo">
     </a>
     <div class="recordDetails">
     <div class="fn">
-    <a href="'.$context->getURL().'">'.$context->name.'</a>
+    <a href="<?php echo $context->getURL() ?>"><?php echo $context->name ?></a>
     </div>
-    '.$title;
-    if (isset($context->phone)) {
-        echo '        <div class="tel">'.$savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php').'</div>'.PHP_EOL;
-    }
-    echo '      </div>
-    <a class="cInfo"" href="'.$context->getURL().'" onclick="return service_officefinder.of_getUID(\''.$context->id.'\');">More Details</a>
+    <?php echo $title ?>
+    <?php if (isset($context->phone)): ?>
+        <div class="tel"><?php echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php') ?></div>
+    <?php endif; ?>
     </div>
-</li>';
+    <a class="cInfo" href="<?php echo $context->getURL() ?>" onclick="return service_officefinder.of_getUID('<?php echo $context->id ?>');">More Details</a>
+    </div>
+</li>
