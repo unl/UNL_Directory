df4a8d814c7bfaa07bf9de84907421fe52ff1e36
Brett Bieber <brett.bieber@gmail.com>
2012-04-24 22:46:24 +0000

    Implement the tree view for the official departments.

--- src/UNL/Officefinder/TreeView.php
+++ src/UNL/Officefinder/TreeView.php
@@ -1,14 +1,22 @@
 <?php
-class UNL_Officefinder_TreeView extends RecursiveIteratorIterator
+class UNL_Officefinder_TreeView extends FilterIterator
 {
     function __construct($options = array())
     {
         // retrieve the left and right value of the $root node  
-        //$root = UNL_Officefinder_Department::getByname('University of Nebraska - Lincoln');
-        $root = UNL_Officefinder_Department::getByname('Office of University Communications');
+        $root = UNL_Officefinder_Department::getByname('University of Nebraska-Lincoln');
+        //$root = UNL_Officefinder_Department::getByname('Office of University Communications');
         $iterator = new UNL_Officefinder_DepartmentList(array($root));
 
-        parent::__construct($iterator, RecursiveIteratorIterator::SELF_FIRST);
+        parent::__construct(new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST));
+    }
+
+    function accept()
+    {
+        if ($this->getInnerIterator()->current()->isOfficialDepartment()) {
+            return true;
+        }
+        return false;
     }
     
 }
--- www/templates/html/Officefinder/TreeView.tpl.php
+++ www/templates/html/Officefinder/TreeView.tpl.php
@@ -1,7 +1,24 @@
-<pre>
+
+<ol>
 <?php
+$depth = 0;
 foreach ($context as $department) {
-    echo '|'.str_repeat('-', $context->getDepth()).$department->name.'<br />';
+    
+    if ($context->getDepth() > $depth) {
+        echo '<ol>';
+    } elseif ($context->getDepth() < $depth) {
+        // Close the current list item
+        echo '</li>';
+        // Loop through until we reach the previous depth
+        echo str_repeat('</ol></li>', $depth - $context->getDepth());
+    } elseif($context->getDepth()==$depth) {
+        echo '</li>';
+    }
+    echo PHP_EOL;
+    echo str_repeat(' ', $context->getDepth());
+    echo '<li class="d'.$context->getDepth().'">'.$department->name;
+    $depth = $context->getDepth();
 }
 ?>
-</pre>
+</li>
+</ol>
