5264c80265908791e5001e3571bc48e76c7eb08f
Brett Bieber <brett.bieber@gmail.com>
2013-06-21 09:34:10 -0500

    Add support for counting the number of roles/appointments a person has

--- src/UNL/Peoplefinder/Person/Roles.php
+++ src/UNL/Peoplefinder/Person/Roles.php
@@ -1,5 +1,5 @@
 <?php
-class UNL_Peoplefinder_Person_Roles extends IteratorIterator
+class UNL_Peoplefinder_Person_Roles extends IteratorIterator implements Countable
 {
     function __construct($options = array())
     {
@@ -13,5 +13,15 @@ class UNL_Peoplefinder_Person_Roles extends IteratorIterator
             $iterator = $options['peoplefinder']->getRoles($options['dn']);
         }
         parent::__construct($iterator);
+    }
+
+    /**
+     * Get the number of roles this person has
+     *
+     * @see Countable::count()
+     */
+    public function count()
+    {
+        return count($this->getInnerIterator());
     }
 }
