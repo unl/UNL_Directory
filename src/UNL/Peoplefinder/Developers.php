f1ee886d598c9e31f8ef98607d624a2c7207f41b
Brett Bieber <brett.bieber@gmail.com>
2013-06-24 16:17:32 -0500

    Coding standards

--- src/UNL/Peoplefinder/Developers.php
+++ src/UNL/Peoplefinder/Developers.php
@@ -6,15 +6,16 @@ class UNL_Peoplefinder_Developers
         'Search',
         'Department',
     );
-    
+
     public $resource;
-    
+
     public $options = array();
-    
-    function __construct($options = array()) {
+
+    function __construct($options = array())
+    {
         $this->options  = $options;
         $this->resource = $this->resources[0];
-        
+
         if (isset($this->options['resource']) ) {
             if (in_array($this->options['resource'], $this->resources)) {
                 $this->resource = $this->options['resource'];
