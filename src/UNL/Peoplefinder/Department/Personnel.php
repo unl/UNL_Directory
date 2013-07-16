ad22c530a9b361a1377cab953144a181826ee831
Brett Bieber <brett.bieber@gmail.com>
2013-07-16 15:28:45 -0500

    Allow retrieving personnel by department ID rather than org_unit

--- src/UNL/Peoplefinder/Department/Personnel.php
+++ src/UNL/Peoplefinder/Department/Personnel.php
@@ -5,11 +5,16 @@ class UNL_Peoplefinder_Department_Personnel extends ArrayIterator
 
     function __construct($mixed)
     {
-        if (is_array($mixed)
-            && !empty($mixed['org_unit'])) {
-            $this->options = $mixed;
-            $peoplefinder  = new UNL_Peoplefinder(array('driver'=>$this->options['driver']));
-            $mixed = $peoplefinder->getHROrgUnitNumberMatches($this->options['org_unit']);
+        if (is_array($mixed)) {
+            if (!empty($mixed['org_unit'])) {
+                $this->options = $mixed;
+                $peoplefinder  = new UNL_Peoplefinder(array('driver'=>$this->options['driver']));
+                $mixed = $peoplefinder->getHROrgUnitNumberMatches($this->options['org_unit']);
+            } elseif (isset($mixed['id'])) {
+                $this->options = $mixed;
+                $department = UNL_Officefinder_Department::getById($mixed['id']);
+                $mixed = $department->getHRDepartment()->getLDAPResults();
+            }
         }
         parent::__construct($mixed);
     }
