9d963328bb7a13b7760ab0b8b8f75ae80117640e
Brett Bieber <brett.bieber@gmail.com>
2013-07-16 16:32:52 -0500

    Add a filter for multiple org unit numbers

---
+++ src/UNL/Peoplefinder/Driver/LDAP/HROrgUnitNumbersFilter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Builds a simple HR primary department filter for records.
+ *
+ * PHP version 5
+ *
+ * @category  Default
+ * @package   UNL_Peoplefinder
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2010 Regents of the University of Nebraska
+ * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
+ * @link      http://peoplefinder.unl.edu/
+ */
+class UNL_Peoplefinder_Driver_LDAP_HROrgUnitNumbersFilter
+{
+    private $_filter;
+
+    /**
+     * Create a filter for HR primary department filtering.
+     *
+     * @param string $hrPrimaryDepartment HR primary department eg:College of Engineering
+     */
+    function __construct($orgUnits)
+    {
+        if (!is_array($orgUnits)) {
+            throw new Exception('Must set primary department.');
+        }
+
+        $this->_filter = '(&(objectClass=person)(|(unlHROrgUnitNumber=';
+        $this->_filter .= implode(')(unlHROrgUnitNumber=', $orgUnits);
+        $this->_filter .= ')))';
+    }
+
+    function __toString()
+    {
+        $this->_filter = UNL_Peoplefinder_Driver_LDAP_Util::wrapGlobalExclusions($this->_filter);
+        return $this->_filter;
+    }
+}
