d65e0781406ce25c1df1b689ae3a91233da5a3d5
Brett Bieber <brett.bieber@gmail.com>
2013-10-25 14:29:36 -0500

    Restore a couple getRaw() references so we can do traditional array operations

--- www/templates/html/Peoplefinder/SearchResults/ByAffiliation.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults/ByAffiliation.tpl.php
@@ -1,7 +1,7 @@
 <?php
 echo '<div id="results_'.$context->affiliation.'" class="results affiliation '.$context->affiliation.'">';
 echo '<h3>'.ucfirst($context->affiliation).'</h3>';
-echo $savvy->render(new UNL_Peoplefinder_SearchResults(array('results'=>$context->results, 'options'=>$context->options )));
+echo $savvy->render(new UNL_Peoplefinder_SearchResults(array('results'=>$context->getRaw('results'), 'options'=>$context->options )));
 if (count($context->like_results)) {
     echo '<div class="likeResults">';
     echo '<h3>similar '.$context->affiliation.' results</h3>';
