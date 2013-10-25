6bee2c45ddfef0bbe6210fdc2bdd60795c7a7d22
Brett Bieber <brett.bieber@gmail.com>
2013-10-25 14:50:47 -0500

    Bring department output controller in line with peoplefinder
    Eventually I hope to consolidate the two frontends

--- www/departments/index.php
+++ www/departments/index.php
@@ -1,7 +1,12 @@
 <?php
-require_once dirname(__FILE__).'/../config.inc.php';
+require_once dirname(__DIR__) . '/config.inc.php';
 
-$options = $_GET;
+$options = array();
+if (strpos($_SERVER['REQUEST_URI'], 'service.php') !== false) {
+    $options = array('format'=>'partial', 'onclick'=>'pf_getUID');
+}
+
+$options = $_GET + $options;
 $options['driver'] = $driver;
 $peoplefinder  = new UNL_Officefinder($options);
 
@@ -15,17 +20,24 @@ header('Access-Control-Allow-Methods: GET, OPTIONS');
 // The X-Requested-With header allows jQuery requests to go through
 header('Access-Control-Allow-Headers: X-Requested-With');
 
+// Exit early so the page isn't fully loaded for options requests
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+    exit();
+}
+
 Savvy_ClassToTemplateMapper::$classname_replacement = 'UNL_';
 $savvy = new Savvy_Turbo();
 $savvy->setTemplatePath(dirname(dirname(__FILE__)).'/templates/html');
 
 switch($peoplefinder->options['format']) {
+    case 'vcard':
+        if ($peoplefinder->output[0] instanceof UNL_Peoplefinder_Record) {
+            header('Content-Type: text/x-vcard');
+            header('Content-Disposition: attachment; filename="'.$peoplefinder->output[0]->sn.', '.$peoplefinder->output[0]->givenName.'.vcf"');
+        }
+        //intentional no break
     case 'json':
     case 'php':
-    case 'vcard':
-    case 'xml':
-        $savvy->addTemplatePath(dirname(dirname(__FILE__)).'/templates/'.$peoplefinder->options['format']);
-        break;
     case 'xml':
         $savvy->addTemplatePath(dirname(dirname(__FILE__)).'/templates/'.$peoplefinder->options['format']);
         break;
@@ -33,8 +45,8 @@ switch($peoplefinder->options['format']) {
         $savvy->setEscape('htmlentities');
         $savvy->addTemplatePath(dirname(dirname(__FILE__)).'/templates/'.$peoplefinder->options['format']);
         break;
-    case 'hcard':
     case 'partial':
+    case 'hcard':
         Savvy_ClassToTemplateMapper::$output_template['UNL_Officefinder'] = 'Peoplefinder-partial';
         // intentional no break
     default:
