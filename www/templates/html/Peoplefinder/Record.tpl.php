8812c33e05195d8bd6f8f7029502df4c7901a8be
Brett Bieber <brett.bieber@gmail.com>
2013-11-26 11:51:30 -0600

    Correct check for presence of campus zip code

--- www/templates/html/Peoplefinder/Record.tpl.php
+++ www/templates/html/Peoplefinder/Record.tpl.php
@@ -92,7 +92,7 @@ if (isset($context->postalAddress)) {
 
     echo '<div class="adr workAdr">
          <span class="type">Work</span>';
-        if (strpos($address['postal-code'], '68588') == 0) {
+        if (strpos($address['postal-code'], '68588') === 0) {
             if ($code = $context->getUNLBuildingCode()) {
                 echo '<span class="street-address">'. str_replace($code, '<a class="location mapurl" href="http://maps.unl.edu/#'.$code.'">'.$code.'</a>', $address['street-address']) . '</span>';
             }
