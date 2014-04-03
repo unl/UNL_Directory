07cb509f9b171ee89c72cbb3bc8687b2909c68c2
Michael Fairchild <mfairchild365@gmail.com>
2014-04-03 11:34:02 -0500

    Remove old search form background
    Replace with a colored band

--- www/templates/html/Peoplefinder/Instructions.tpl.php
+++ www/templates/html/Peoplefinder/Instructions.tpl.php
@@ -1,4 +1,4 @@
-<div class="wdn-band search-container">
+<div class="wdn-band wdn-light-neutral-band search-container">
     <div class="wdn-inner-wrapper wdn-inner-padding-none">
         <?php
         if (isset($context->options['adv'])) {
