91fef73f1ca711fb7e5729389b9fd379165194b7
Michael Fairchild <mfairchild365@gmail.com>
2014-05-07 15:45:40 -0500

    Make the filter lists focusable

--- www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
@@ -4,14 +4,14 @@
         <form method="post" action="#" class="filters">
             <fieldset class="affiliation">
                 <legend tabindex="0" role="button" atrai-controls="filters_affiliation">By Affiliation (<span class="toggle">Expand</span>)</legend>
-                <ol id="filters_affiliation" role="region"  aria-expanded="false">
+                <ol id="filters_affiliation" role="region" tabindex="-1" aria-expanded="false">
                    <li>
                    </li>
                 </ol>
             </fieldset>
             <fieldset class="department">
                 <legend tabindex="0" role="button" atrai-controls="filters_department">By Department (<span class="toggle">Expand</span>)</legend>
-                <ol id="filters_department" role="region"  aria-expanded="false">
+                <ol id="filters_department" role="region" tabindex="-1" aria-expanded="false">
                     <li>
                     </li>
                 </ol>
