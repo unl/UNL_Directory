d2604c3f4a90fcfbf5db21a311362f4f107debd6
Michael Fairchild <mfairchild365@gmail.com>
2014-05-08 09:17:47 -0500

    Don't stack event listeners

--- www/scripts/filters.js
+++ www/scripts/filters.js
@@ -1,32 +1,38 @@
 var filters = function() {
 	var departments = [];
 	var affiliations = [];
+
+	/**
+	 * Expand / collapse filters
+	 */
+	WDN.jQuery(".filters legend").on('click keypress', function (e) {
+		if (e.keyCode !== undefined && !(e.keyCode == 0 || e.keyCode == 13)) {
+			//Not a space or enter key press
+			return;
+		}
+		$header  = WDN.jQuery(this);
+		$content = $header.next();
+		$content.slideToggle(500, function () {
+			if ($content.is(":visible")) {
+				//Expanded
+				$header.children('.toggle').text("Collapse");
+				$content.attr('aria-expanded', 'true');
+				$content.focus();
+			} else {
+				//Collapsed
+				$header.children('.toggle').text("Expand");
+				$content.attr('aria-expanded', 'false');
+			}
+		});
+	});
+
 	return {
 		initialize : function() {
 			WDN.jQuery('form.filters fieldset ol').empty().parents('form').addClass('loading');
 			WDN.jQuery('#filters').show();
 			filters.findClasses();
-			WDN.jQuery(".filters legend").on('click keypress', function (e) {
-				if (e.keyCode !== undefined && !(e.keyCode == 0 || e.keyCode == 13)) {
-					//Not a space or enter key press
-					return;
-				}
-				$header  = WDN.jQuery(this);
-				$content = $header.next();
-				$content.slideToggle(500, function () {
-					if ($content.is(":visible")) {
-						//Expanded
-						$header.children('.toggle').text("Collapse");
-						$content.attr('aria-expanded', 'true');
-						$content.focus();
-					} else {
-						//Collapsed
-						$header.children('.toggle').text("Expand");
-						$content.attr('aria-expanded', 'false');
-					}
-				});
-			});
 
+			//Hide the filters if there is only a few results (on mobile)
 			WDN.jQuery('#filters').removeClass('few-results');
 			WDN.jQuery('#filters').removeClass('many-results');
 			var total = WDN.jQuery('.ppl_Sresult');
