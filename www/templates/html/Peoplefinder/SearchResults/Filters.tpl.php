e027a708ef6a95a8a2c887da0d1a37c0954c911f
Michael Fairchild <mfairchild365@gmail.com>
2014-05-08 11:35:21 -0500

    spell aria correctly

--- www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
+++ www/templates/html/Peoplefinder/SearchResults/Filters.tpl.php
@@ -3,14 +3,14 @@
         <h4 class="formal">Filter Results</h4>
         <form method="post" action="#" class="filters">
             <fieldset class="affiliation">
-                <legend tabindex="0" role="button" atrai-controls="filters_affiliation">By Affiliation (<span class="toggle">Expand</span>)</legend>
+                <legend tabindex="0" role="button" aria-controls="filters_affiliation">By Affiliation (<span class="toggle">Expand</span>)</legend>
                 <ol id="filters_affiliation" role="region" tabindex="-1" aria-expanded="false">
                    <li>
                    </li>
                 </ol>
             </fieldset>
             <fieldset class="department">
-                <legend tabindex="0" role="button" atrai-controls="filters_department">By Department (<span class="toggle">Expand</span>)</legend>
+                <legend tabindex="0" role="button" aria-controls="filters_department">By Department (<span class="toggle">Expand</span>)</legend>
                 <ol id="filters_department" role="region" tabindex="-1" aria-expanded="false">
                     <li>
                     </li>
