ca3beac864270f761df587c9bae32d5f661cb30e
Brett Bieber <brett.bieber@gmail.com>
2014-05-23 09:37:51 -0500

    Update global ignore

--- src/UNL/Peoplefinder/Driver/LDAP/Util.php
+++ src/UNL/Peoplefinder/Driver/LDAP/Util.php
@@ -113,7 +113,7 @@ class UNL_Peoplefinder_Driver_LDAP_Util
         return 
             '(&'
             . $filter
-            . '(!(|(eduPersonPrimaryAffiliation=guest)(uid=msanders3)))' .
+            . '(!(|(eduPersonPrimaryAffiliation=guest)(uid=jfrancisco3)))' .
             ')';
     }
 }
