3472cc33c42591a89b965344ba6bfa316a5f59b2
Kevin Abel <kevin.abel.0@gmail.com>
2015-09-28 15:55:21 -0500

    Use serialization methods in php render to support better transports
    Services downstream should not have to have special library classes to
    deserialize and use the service information. Full backwards
    compatibility is offered and extended with QS params

--- www/templates/php/Peoplefinder/Record.tpl.php
+++ www/templates/php/Peoplefinder/Record.tpl.php
@@ -1,2 +1,8 @@
 <?php
-echo serialize($context->getRawObject());
+if (isset($_GET['unsafe']) && $_GET['unsafe']) {
+	echo serialize($context);
+} elseif (isset($_GET['multivalue']) && $_GET['multivalue']) {
+	echo $context->serialize(UNL_Peoplefinder_Record::SERIALIZE_VERSION_SAFE_MULTIVALUE);
+} else {
+	echo $context->serialize(UNL_Peoplefinder_Record::SERIALIZE_VERSION_SAFE);
+}
