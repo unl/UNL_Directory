e41a3c5a1feb8e70c7f65e3229ddacc43b55090a
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 17:52:09 -0500

    Refactor the department delete form templates

--- www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
@@ -1,6 +1,6 @@
-<a href="#" class="action delete" title="Delete" onclick="if (confirm('Are you sure? This will delete the alias.')) document.getElementById('deletealias_<?php echo $context->department_id.$context->name; ?>').submit(); else return false;">Delete</a>
-<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deletealias_<?php echo $context->department_id.$context->name; ?>" style="width:120px;" class="delete">
+<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deletealias_<?php echo $context->department_id.$context->name; ?>" class="delete">
     <input type="hidden" name="_type" value="delete_dept_alias" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
     <input type="hidden" name="name" value="<?php echo $context->name; ?>" />
+	<button class="icon-trash" type="submit" onclick="if (!confirm('Are you sure? This will delete the alias.')) return false;"><span class="wdn-text-hidden">Delete</span></button>
 </form>
