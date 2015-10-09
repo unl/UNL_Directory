f4913a5b8cf03ee9d435be4f08efae3e65e55c3f
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 17:51:32 -0500

    Refactor the department user forms

--- www/templates/editing/Officefinder/Department/User/AddForm.tpl.php
+++ www/templates/editing/Officefinder/Department/User/AddForm.tpl.php
@@ -1,6 +1,10 @@
 <form action="<?php echo $context->getURL(); ?>" method="post" id="adduser_<?php echo $context->id; ?>" class="add">
     <input type="hidden" name="_type" value="add_dept_user" />
     <input type="hidden" name="department_id" value="<?php echo $context->id; ?>" />
-    <input type="text" name="uid" />
-    <input type="submit" value="Add user" />
+    <div class="wdn-input-group">
+    	<input type="text" name="uid" />
+    	<span class="wdn-input-group-btn">
+    		<input type="submit" value="Add" />
+    	</span>
+    </div>
 </form>
--- www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php
@@ -1,6 +1,6 @@
-<a href="#" class="action delete" title="Delete" onclick="if (confirm('Are you sure? This will remove editing access for this user.')) document.getElementById('deleteuser_<?php echo $context->department_id.$context->uid; ?>').submit(); else return false;">Delete</a>
-<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deleteuser_<?php echo $context->department_id.$context->uid; ?>" style="width:120px;" class="delete">
+<form action="<?php echo $parent->parent->context->getURL(); ?>" method="post" id="deleteuser_<?php echo $context->department_id.$context->uid; ?>" class="delete">
     <input type="hidden" name="_type" value="delete_dept_user" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
     <input type="hidden" name="uid" value="<?php echo $context->uid; ?>" />
+    <button class="icon-trash" type="submit" onclick="if (!confirm('Are you sure? This will remove editing access for this user.')) return false;"><span class="wdn-text-hidden">Delete</span></button>
 </form>
