3e00d72350f92e89fc7bb783bec1b0a6bf81f349
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 18:06:32 -0500

    Refactor the department list inner template

--- www/templates/html/Officefinder/DepartmentList/ListItem.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/ListItem.tpl.php
@@ -1,30 +1,31 @@
 <?php
 $title = '';
-$parent = $context->getParent();
-if (!$context->isOfficialDepartment()) {
-    if ($parent) {
-        $title = '<div class="title">('.$parent->name.')</div>';
-    }
-}
 $li_class = 'dep_result parent_'.$context->id;
+
+$parent = $context->getParent();
 if ($parent) {
     $li_class .= ' parent_'.$parent->id;
+
+    if (!$context->isOfficialDepartment()) {
+        $title = $parent->name;
+    }
 }
 ?>
-<li class="<?php echo $li_class ?>">
+<li class="<?php echo $li_class ?>" tabindex="0" data-href="<?php echo $context->getURL() ?>">
     <div class="overflow">
-    <a class="planetred_profile" href="<?php echo $context->getURL() ?>">
-    <img alt="Generic Icon" src="<?php echo UNL_Peoplefinder::getURL() ?>images/organization40.png" class="profile_pic small photo">
-    </a>
-    <div class="recordDetails">
-    <div class="fn">
-    <a href="<?php echo $context->getURL() ?>"><?php echo $context->name ?></a>
-    </div>
-    <?php echo $title ?>
-    <?php if (isset($context->phone)): ?>
-        <div class="tel"><?php echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php') ?></div>
-    <?php endif; ?>
-    </div>
-    <a class="cInfo" href="<?php echo $context->getURL() ?>">More Details</a>
+        <div class="profile_pic" href="<?php echo $context->getURL() ?>">
+            <img alt="Building Image" src="<?php echo $context->getImageUrl(UNL_Peoplefinder_Record_Avatar::AVATAR_SIZE_SMALL) ?>" class="photo" />
+        </div>
+        <div class="recordDetails">
+            <div class="fn">
+                <a href="<?php echo $context->getURL() ?>"><?php echo $context->name ?></a>
+            </div>
+            <?php if ($title): ?>
+                <div class="title"><?php echo $title ?></div>
+            <?php endif; ?>
+            <?php if (!empty($context->phone)): ?>
+                <div class="tel"><?php echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php') ?></div>
+            <?php endif; ?>
+        </div>
     </div>
 </li>
