45c21767c1c9e227bb157aaaf76f87de34d7828b
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 12:42:12 -0500

    Search form improvements
    Target the help to a new tab/window and add templates for standard
    generated messages.

--- www/templates/html/Peoplefinder/StandardForm.tpl.php
+++ www/templates/html/Peoplefinder/StandardForm.tpl.php
@@ -10,10 +10,36 @@
     }
     ?>
     <div class="wdn-input-group">
-        <input tabindex="0" type="text" autofocus placeholder="Herbie Husker" value="<?php echo $default; ?>" id="q" name="q" title="Enter a name to begin your search" class="q" />
+        <input tabindex="0" type="text" autofocus placeholder="Herbie Husker" value="<?php echo $default; ?>" id="q" name="q" title="Enter a name to begin your search" />
         <span class="wdn-input-group-btn">
             <button name="submitbutton" type="submit" value="Search" title="Search" class="button wdn-icon-search"></button>
         </span>
     </div>
 </form>
-<p><a href="<?php echo UNL_Peoplefinder::getURL() ?>help/" class="wdn-button">Help</a></p>
+<p><a href="<?php echo UNL_Peoplefinder::getURL() ?>help/" class="wdn-button" target="_blank">Help</a></p>
+
+<script id="annotateTemplate" type="text/x-jsrender">
+<div title="Leave notes on the listing for {{:preferredName}}" class="wdn_annotate" id="directory_{{:uid}}"></div>
+</script>
+
+<script id="noticeTemplate" type="text/x-jsrender">
+<div class="wdn_notice">
+    <div class="close">
+        <a href="#" title="Close this notice">Close this notice</a>
+    </div>
+    <div class="message">
+        <p class="title">We automatically tried some other searches for you.</p>
+        <p>
+            Your original search for <span>{{:originalSearch}}</span> did not return any results.
+            So we tried a few more advanced searches and below is what we found for <span>First Name: {{:firstName}} AND Last Name: {{:lastName}}</span>.</p>
+    </div>
+</div>
+</script>
+
+<script id="genericErrorTemplate" type="text/x-jsrender">
+<p class="error">Something went wrong. Please try again later.</p>
+</script>
+
+<script id="queryLengthTemplate" type="text/x-jsrender">
+<p>Please enter more information, your query must be at least 3 characters long.</p>
+</script>
