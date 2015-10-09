4a1c7d2249ad9daf6653f83b6bca624729428990
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 17:59:15 -0500

    Simplify the PF Department template

--- www/templates/html/Peoplefinder/Department.tpl.php
+++ www/templates/html/Peoplefinder/Department.tpl.php
@@ -1,25 +1,5 @@
-<?php
-if (count($context)) {
-    echo '<h3>'.$context->name.'</h3>';
-    $building = '';
-    if (isset($context->building)) {
-        $building = $context->building;
-        $bldgs = new UNL_Common_Building();
-        if ($bldgs->buildingExists($context->building)) {
-            $sd = new UNL_Geography_SpatialData_Campus();
-            $building = '<a href="'.$sd->getMapUrl($context->building).'">'.htmlentities($bldgs->codes[$context->building]).'</a>';
-        }
-        $building = "<span class='location'>$building</span>";
-    }
-    echo "<p>{$context->room} {$building}<br />{$context->city}, {$context->state} {$context->postal_code}</p>";
-//    if ($context->hasChildren()) {
-//        echo 'Sub-departments:<ul>';
-//        foreach ($context->getChildren() as $child) {
-//            echo '<li><a href="'.UNL_Officefinder::getURL().'?d='.urlencode($child->name).'">'.htmlentities($child->name).'</a></li>';
-//        }
-//        echo '</ul>';
-//    }
-    echo $savvy->render($context, 'Peoplefinder/Department/Personnel.tpl.php');
-} else {
-    echo 'No results could be found.';
-}
+<?php if (count($context)): ?>
+	<?php echo $savvy->render($context, 'Peoplefinder/Department/Personnel.tpl.php') ?>
+<?php else: ?>
+    No results could be found.
+<?php endif; ?>
