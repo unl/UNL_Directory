dfd3da0fc1774446c0397629ccfed8b5aff5b764
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 17:42:26 -0500

    Move add alias form to consistent location

--- www/templates/editing/Officefinder/Department/Alias/AddForm.tpl.php
+++ www/templates/editing/Officefinder/Department/Alias/AddForm.tpl.php
@@ -1,6 +1,10 @@
 <form action="<?php echo $context->getURL(); ?>" method="post" id="addalias_<?php echo $context->id; ?>" class="add">
     <input type="hidden" name="_type" value="add_dept_alias" />
     <input type="hidden" name="department_id" value="<?php echo $context->id; ?>" />
-    <input type="text" name="name" />
-    <input type="submit" value="Add alias" />
+    <div class="wdn-input-group">
+    	<input type="text" name="name" />
+    	<span class="wdn-input-group-btn">
+    		<input type="submit" value="Add" />
+    	</span>
+    </div>
 </form>
