71e8f36aaae4490fbfc7199bae92eee4740bfdc2
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 12:45:20 -0500

    Peoplfinder template should not normally start_session
    Fixes logic with the Officefinder injection, record rendering, and
    javascript static container.

--- www/templates/html/Peoplefinder.tpl.php
+++ www/templates/html/Peoplefinder.tpl.php
@@ -13,7 +13,7 @@ $page->__params['class']['value'] = implode(' ', $classes);
 
 $page->head .= $savvy->render(null, 'static/head.tpl.php');
 
-if ($context->options['view'] != 'alphalisting' && UNL_Officefinder::getUser()
+if ($controller instanceof UNL_Officefinder && $context->options['view'] != 'alphalisting' && UNL_Officefinder::getUser()
     && 
         (
         UNL_Officefinder::isAdmin(UNL_Officefinder::getUser()) || count(new UNL_Officefinder_User_Departments(array('uid'=>UNL_Officefinder::getUser())))
@@ -49,14 +49,16 @@ if (in_array($context->options['view'], array('instructions', 'search'))) {
     $page->maincontentarea = $savvy->render($context->output, $outputTemplate);
 } else {
     //Wrap everything else
-    $page->maincontentarea = '<div class="wdn-band results-container"><div class="wdn-inner-wrapper wdn-inner-padding-sm">' . $savvy->render($context->output, $outputTemplate) . '</div></div>';
+    $page->maincontentarea = '<div class="wdn-band record-container"><div class="wdn-inner-wrapper wdn-inner-padding-sm">' . $savvy->render($context->output, $outputTemplate) . '</div></div>';
 }
+
+// add entry-point scripts
+$page->maincontentarea .= $savvy->render(null, 'static/after-main.tpl.php');
 
 $page->contactinfo = $savvy->render(null, 'static/contact-info.tpl.php');
 
 $page->leftcollinks = $savvy->render(null, 'static/op-footer.tpl.php');
 
 $page->footercontent = $savvy->render(null, 'static/footer.tpl.php');
-$page->footercontent .= $savvy->render($context, 'CorrectionForm.tpl.php');
 
 echo $page;
