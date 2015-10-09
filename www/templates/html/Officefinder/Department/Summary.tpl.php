f61ca921f2ce55c07af1ffa136a9b2dd4d4179e2
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-09 18:01:45 -0500

    Refactor the department summary template

--- www/templates/html/Officefinder/Department/Summary.tpl.php
+++ www/templates/html/Officefinder/Department/Summary.tpl.php
@@ -1,109 +1,113 @@
 <?php
-$userCanEdit = false;
+$userCanEdit = $userCanDelete = false;
+
 if ($controller->options['view'] != 'alphalisting') {
     $userCanEdit = $context->userCanEdit(UNL_Officefinder::getUser());
+	// Only allow Admins to delete "official" SAP departments
+    $userCanDelete = empty($context->org_unit) || UNL_Officefinder::isAdmin(UNL_Officefinder::getUser());
 }
+
+$officialParent = $context->getOfficialParent();
+
+$encodedEmail = '';
+if (!empty($context->email)) {
+    // attempt to curb lazy email harvesting bots
+    $encodedEmail = htmlentities($context->getRaw('email'), ENT_QUOTES | ENT_HTML5);
+}
+
+$onlySummary = $context->isSummaryView();
 ?>
-<div class="departmentInfo"
-        <?php 
-         if (!empty($context->org_unit)) {
-                echo ' data-orgUnit="'.$context->org_unit.'"';
-            }
-        ?>
-    >
-    <?php
-    $image_url = 'http://maps.unl.edu/images/building/icon_md.png';
-    if (!empty($context->building)) {
-        $bldgs = new UNL_Common_Building();
-        if ($bldgs->buildingExists($context->building)) {
-            $image_url = 'http://maps.unl.edu/building/'.urlencode($context->building).'/image/1/md';
-        }
-    }
-    ?>
-    
-    <?php 
-    if ($userCanEdit) {
-    	echo '<a class="minibutton edit" href="'.$context->getURL().'?format=editing" title="Edit"><span class="icon">Edit<span></a>
-        	<div class="action_control">
-            <h4>Edit this Department</h4>
-    		<div class="form"></div>';
-            if (!isset($context->org_unit) || UNL_Officefinder::isAdmin(UNL_Officefinder::getUser(true))) {
-            	// Only allow Admins to delete "official" SAP departments
-            	include dirname(__FILE__).'/../../../editing/Officefinder/Department/DeleteForm.tpl.php';
-            }
-        echo '</div>';
-    }
-    ?>
-    <div class="vcard office">
-        <img alt="Building Image" src="<?php echo $image_url; ?>" width="100" height="100" class="frame photo">
-        <h4 class="fn org">
-            <a href="<?php echo $context->getURL();?>">
-            <?php
-            	echo $context->name;
-            ?>
-            </a>
-            <a class="permalink" href="<?php echo $context->getURL();?>">link</a>
-        </h4>
-        <div class="vcardInfo">
-            <div class="adr label">
-                <span class="type">Address</span>
-                <span class="room">
-                    <?php 
-                    if ($context->building) {
-                        echo $context->room.' <a class="location mapurl" href="http://maps.unl.edu/#'.$context->building.'" onclick="WDN.jQuery.colorbox({href:\'http://maps.unl.edu/'.$context->building.'?format=staticgooglemapsv2&size=400x400&zoom=17\', width:\'460px\', height:\'490px\'});return false;">'.$context->building.'</a>'; 
-                    }
-                    ?>
-                </span>
-                <?php
-                if (!empty($context->address)) {
-                    echo "<span class='street-address'>" . $context->address . "</span>";
-                }
-                if (!empty($context->city)) {
-                    echo "<span class='locality'>" . $context->city . "</span>";
-                }
-                if (!empty($context->state)) {
-                    echo "<span class='region'>" . $context->state . "</span>";
-                }
-                if (!empty($context->postal_code)) {
-                    echo "<span class='postal-code'>" . $context->postal_code . "</span>";
-                }
-                ?>
-                <span class='country-name'>USA</span>
-            </div>
-            
-            <?php if (isset($context->phone)): ?>
-            <div class="tel">
-                <span class="voice">Phone:
-                    <?php
-                    echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php');
-                    ?>
+<div class="departmentInfo">
+    <div class="vcard office<?php if($onlySummary): ?> card<?php endif; ?>">
+        <div class="card-profile">
+            <img alt="Building Image" src="<?php echo $context->getImageURL(UNL_Peoplefinder_Record_Avatar::AVATAR_SIZE_LARGE); ?>" class="photo" />
+        </div>
+        <div class="vcardInfo<?php if($onlySummary): ?> card-content<?php endif; ?>">
+            <?php if (!$onlySummary): ?>
+                <h1 class="headline">
+            <?php else: ?>
+                <div class="headline">
+            <?php endif; ?>
+                <a class="permalink" href="<?php echo $context->getURL() ?>">
+                    <span class="fn org"><?php echo $context->name ?></span>
+                    <span class="icon-link"></span>
+                </a>
+            <?php if ($onlySummary): ?>
+                </div>
+            <?php else: ?>
+                </h1>
+            <?php endif; ?>
+
+            <?php if (!$context->isOfficialDepartment()): ?>
+                 <div class="title"><?php echo $officialParent->name ?></div>
+            <?php endif; ?>
+
+            <?php if ($context->hasAddress()): ?>
+                <div class="adr work itemprop icon-map-pin">
+                    <span class="type">Address</span>
+                    <?php if ($context->building): ?>
+                        <span class="room">
+                            <a href="https://maps.unl.edu/<?php echo $context->building ?>"><?php echo $context->building ?></a>
+                            <?php echo $context->room ?>
+                        </span>
+                    <?php endif; ?>
+                    <?php if (!empty($context->address)): ?>
+                        <span class="street-address"><?php echo $context->address ?></span>
+                    <?php endif; ?>
+                    <?php if (!empty($context->city)): ?>
+                        <span class='locality'><?php echo $context->city ?></span>
+                    <?php endif; ?>
+                    <?php if (!empty($context->state)): ?>
+                        <?php echo $savvy->render($context->state, 'Peoplefinder/Record/Region.tpl.php') ?>
+                    <?php endif; ?>
+                    <?php if (!empty($context->postal_code)): ?>
+                        <span class='postal-code'><?php echo $context->postal_code ?></span>
+                    <?php endif; ?>
+                    <div class="country-name">USA</div>
+                </div>
+            <?php endif; ?>
+
+            <?php if (!empty($context->phone)): ?>
+            <div class="tel work icon-phone itemprop">
+                <span class="voice">
+                    <span class="type">Phone:</span>
+                    <?php echo $savvy->render($context->phone, 'Peoplefinder/Record/TelephoneNumber.tpl.php') ?>
                 </span>
             </div>
             <?php endif; ?>
-            <?php if (isset($context->fax)): ?>
-            <div class="tel">
-                <span class="fax">Fax:
-                    <?php
-                    echo $savvy->render($context->fax, 'Peoplefinder/Record/TelephoneNumber.tpl.php');
-                    ?>
+            <?php if (!empty($context->fax)): ?>
+            <div class="tel work icon-print itemprop">
+                <span class="fax">
+                    <span class="type">Fax:</span>
+                    <?php echo $savvy->render($context->fax, 'Peoplefinder/Record/TelephoneNumber.tpl.php'); ?>
                 </span>
             </div>
             <?php endif; ?>
-            
-            
-            <?php if (isset($context->email)): ?>
-            <div class="email">
-                <span class="email">
-                   <a class="email" href="mailto:<?php echo $context->email; ?>"><?php echo $context->email; ?></a>
-                </span>
+            <?php if ($encodedEmail): ?>
+            <div class="icon-email itemprop">
+                   <a class="email" href="mailto:<?php echo $encodedEmail ?>" itemprop="email"><?php echo $encodedEmail ?></a>
             </div>
             <?php endif; ?>
-            <?php if (isset($context->website)): ?>
-            <div class="url">
-                <span class="url">
-                   <a class="url" href="<?php echo $context->website; ?>"><?php echo $context->website; ?></a>
-                </span>
+
+            <?php if (!empty($context->website)): ?>
+            <div class="icon-website itemprop">
+                <a class="url" href="<?php echo $context->website; ?>"><?php echo $context->website; ?></a>
             </div>
+            <?php endif; ?>
+
+            <?php if ($context->isOfficialDepartment()): ?>
+                <div class="icon-hierarchy itemprop">
+                    <?php echo $context->org_unit ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($userCanEdit): ?>
+                <div class="vcard-tools">
+                    <a href="<?php echo $context->getURL() . '/edit' ?>" class="icon-pencil">Edit</a>
+                    <?php if ($userCanDelete): ?>
+                        <button class="icon-trash">Delete</button>
+                    <?php endif; ?>
+                </div>
             <?php endif; ?>
         </div>
     </div>
