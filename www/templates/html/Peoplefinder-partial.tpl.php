062b33e5dfeae8b29b43d5cb2c686804fea3e87a
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-16 17:56:53 -0500

    Revert "Fix for peoplefinder partial hopefully"
    This reverts commit 3aae4e2e90334b2dc3343aba8a3938b3ef8e63d3. As it
    breaks all AJAX related interaction.

--- www/templates/html/Peoplefinder-partial.tpl.php
+++ www/templates/html/Peoplefinder-partial.tpl.php
@@ -1,4 +1,3 @@
 <?php
 // This template is used in the hcard outputs for pulling snippets of HTML
-//var_dump($context);
-echo $savvy->render($context->output->record, 'Peoplefinder/Record.tpl.php');
+echo $savvy->render($context->output);
