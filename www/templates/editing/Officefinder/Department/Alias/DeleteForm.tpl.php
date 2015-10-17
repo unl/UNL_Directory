a7e167d56488beac16467df3f6d19f00863cc8d2
Kevin Abel <kevin.abel.0@gmail.com>
2015-10-16 19:01:30 -0500

    Color delete buttons red

--- www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/Alias/DeleteForm.tpl.php
@@ -2,5 +2,5 @@
     <input type="hidden" name="_type" value="delete_dept_alias" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
     <input type="hidden" name="name" value="<?php echo $context->name; ?>" />
-	<button class="icon-trash" type="submit" onclick="if (!confirm('Are you sure? This will delete the alias.')) return false;"><span class="wdn-text-hidden">Delete</span></button>
+	<button class="icon-trash wdn-button-brand" type="submit" onclick="if (!confirm('Are you sure? This will delete the alias.')) return false;"><span class="wdn-text-hidden">Delete</span></button>
 </form>
--- www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php
+++ www/templates/editing/Officefinder/Department/User/DeleteForm.tpl.php
@@ -2,5 +2,5 @@
     <input type="hidden" name="_type" value="delete_dept_user" />
     <input type="hidden" name="department_id" value="<?php echo $context->department_id; ?>" />
     <input type="hidden" name="uid" value="<?php echo $context->uid; ?>" />
-    <button class="icon-trash" type="submit" onclick="if (!confirm('Are you sure? This will remove editing access for this user.')) return false;"><span class="wdn-text-hidden">Delete</span></button>
+    <button class="icon-trash wdn-button-brand" type="submit" onclick="if (!confirm('Are you sure? This will remove editing access for this user.')) return false;"><span class="wdn-text-hidden">Delete</span></button>
 </form>
