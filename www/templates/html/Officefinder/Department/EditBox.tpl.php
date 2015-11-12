005172bd96e2f7857373129ec8851ba3a3f62447
Kevin Abel <kabel2@unl.edu>
2015-11-11 19:48:18 -0600

    Add the My Departments list to the end of the department EditBox

--- www/templates/html/Officefinder/Department/EditBox.tpl.php
+++ www/templates/html/Officefinder/Department/EditBox.tpl.php
@@ -1,9 +1,9 @@
 <div id="editBox" data-department-id="<?php echo $context->id ?>">
-	<div class="aliases">
+	<div class="aliases" aria-live="polite">
 		<?php echo $savvy->render($context->getAliases()) ?>
 		<?php echo $savvy->render($context, 'Officefinder/Department/Alias/AddForm.tpl.php') ?>
 	</div>
-	<div class="users">
+	<div class="users" aria-live="polite">
 		<?php echo $savvy->render($context->getUsers()) ?>
 		<?php echo $savvy->render($context, 'Officefinder/Department/User/AddForm.tpl.php') ?>
 	</div>
@@ -12,3 +12,7 @@
 		<?php echo $savvy->render($context, 'Officefinder/Department/EditForm.tpl.php') ?>
 	</div>
 </div>
+<aside>
+	<h2>My Departments</h2>
+	<?php echo $savvy->render(new UNL_Officefinder_User_Departments(), 'Officefinder/User/DepartmentList.tpl.php') ?>
+</aside>
