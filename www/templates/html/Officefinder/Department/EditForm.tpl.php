a78c504bc26488ed1ba6609c5d355b9101b5d295
Kevin Abel <kabel2@unl.edu>
2015-11-11 19:49:36 -0600

    Clean up the EditForm template

--- www/templates/html/Officefinder/Department/EditForm.tpl.php
+++ www/templates/html/Officefinder/Department/EditForm.tpl.php
@@ -1,76 +1,61 @@
-<form id="editdepartment_<?php echo $context->id; ?>" action="<?php echo $context->getURL(); ?>" class="edit" method="post">
+<?php
+if ($context->id) {
+    $actionUrl = $context->getURL();
+} elseif (isset($context->options['parent_id'])) {
+    $actionUrl = $context->getParent()->getURL();
+}
+
+$buildings = UNL_Peoplefinder_Record_Avatar::getBuildings();
+$emptyBuildingSelection = empty($context->building) || !isset($buildings[$context->building]);
+
+$fields = [
+    'room' => '',
+    'address' => 'Street address for USPS mailings (do not use university building codes)',
+    'city' => '',
+    'state' => '',
+    'postal_code' => '10 characters max, eg 68588-0424',
+    'phone' => 'Use the full number with area code',
+    'fax' => 'Use the full number with area code',
+    'email' => '',
+    'website' => 'Always include http://'
+];
+?>
+<form id="editdepartment_<?php echo $context->id; ?>" action="<?php echo $actionUrl ?>" class="edit" method="post">
     <input type="hidden" name="_type" value="department" />
     <?php if (isset($context->id)): ?>
-    <input type="hidden" name="id" value="<?php echo $context->id; ?>" />
+        <input type="hidden" name="id" value="<?php echo $context->id; ?>" />
     <?php elseif (isset($context->options['parent_id'])) : ?>
-    <input type="hidden" name="parent_id" value="<?php echo (int)$context->options['parent_id']; ?>" />
-    <?php else :
-            throw new Exception('You must edit a record, or set a new parent.'); 
-          endif; ?>
-    <fieldset>
-        <legend>Department Details</legend>
-        <ol>
-            <li>
-                <label for="name">
-                    <span class="required">*</span>
-                    Name
-                    <span class="helper">The official name of this entity</span>
-                </label>
-                <input type="text" id="name" name="name" value="<?php echo $context->name; ?>" />
-            </li>
-            <li>
-                <label for="building">Campus Building</label>
-                <select name="building" id="building">
-                    <option value="">N/A</option>
-                    <?php
-                    $found = false;
-                    foreach (array('City', 'East') as $campus): ?>
-                    <optgroup label="<?php echo $campus; ?> Campus">
-                        <?php
-                        $class = 'UNL_Common_Building_'.$campus;
-                        $buildings = new $class();
-                        asort($buildings->codes);
-                        foreach ($buildings->codes as $code=>$name):
-                            $selected = '';
-                            if ($code == $context->building) {
-                                $found = true;
-                                $selected = 'selected="selected"';
-                            }
-                        ?>
-                        <option value="<?php echo htmlspecialchars($code, ENT_QUOTES); ?>" <?php echo $selected; ?>><?php echo htmlspecialchars($name).' ('.htmlspecialchars($code).')'; ?></option>
-                    
-                    <?php endforeach; ?>
-                    </optgroup>
-                    <?php 
-                    endforeach;
-                    if (!$found) {
-                        echo '<option value="'.$context->building.'" selected="selected">'.$context->building.' (Unknown building. Please correct if possible.)</option>'.PHP_EOL;
-                    }
-                    ?>
-                </select>
-            </li>
-            <?php foreach (array('room'        => '',
-                                 'address'     => 'Street address for USPS mailings',
-                                 'city'        => '',
-                                 'state'       => '',
-                                 'postal_code' => '10 characters max, eg 68588-0424',
-                                 'phone'       => 'Use the full number with area code',
-                                 'fax'         => 'Use the full number with area code',
-                                 'email'       => '',
-                                 'website'     => 'Always include http://') as $var=>$description): ?>
+        <input type="hidden" name="parent_id" value="<?php echo (int)$context->options['parent_id']; ?>" />
+    <?php endif; ?>
+    <ol>
+        <li>
+            <label for="name">
+                <span class="required">*</span>
+                Name
+                <span class="helper">The official name of this entity</span>
+            </label>
+            <input type="text" id="name" name="name" value="<?php echo $context->name; ?>" />
+        </li>
+        <li>
+            <label for="building">Campus Building</label>
+            <select name="building" id="building">
+                <option value=""<?php if ($emptyBuildingSelection): ?> selected<?php endif; ?>>N/A</option>
+                <?php foreach ($buildings as $code => $name): ?>
+                    <option value="<?php echo $savvy->escape($code) ?>"<?php if (!$emptyBuildingSelection && $code === $context->building): ?> selected<?php endif; ?>><?php echo $savvy->escape($name) . ' [' . $savvy->escape($code) . ']' ?></option>
+                <?php endforeach; ?>
+            </select>
+        </li>
+        <?php foreach ($fields as $var => $description): ?>
             <li>
                 <label for="<?php echo $var; ?>">
-                    <?php
-                    echo ucwords(str_replace('_', ' ', $var));
-                    if (!empty($description)) {
-                        echo '<span class="helper">'.$description.'</span>';
-                    }
-                    ?>
+                    <?php echo ucwords(str_replace('_', ' ', $var)) ?>
+                    <?php if (!empty($description)): ?>
+                        <span class="helper"><?php echo $description ?></span>
+                    <?php endif; ?>
                 </label>
-                <input type="text" id="<?php echo $var; ?>" name="<?php echo $var; ?>" value="<?php echo $context->$var; ?>" />
+                <input type="text" name="<?php echo $var ?>" value="<?php echo $context->$var; ?>" />
             </li>
-            <?php endforeach; ?>
-        </ol>
-    </fieldset>
-    <input type="submit" name="submit" value="Submit" />
+        <?php endforeach; ?>
+    </ol>
+    <input type="submit" value="Submit" />
 </form>
