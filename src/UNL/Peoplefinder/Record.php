f1b0b5598ef9592c992d52ff2ef5cf326c54334d
Kevin Abel <kabel2@unl.edu>
2016-01-21 11:35:39 -0600

    Don't inject properties to Record objects without uid

--- src/UNL/Peoplefinder/Record.php
+++ src/UNL/Peoplefinder/Record.php
@@ -701,16 +701,18 @@ class UNL_Peoplefinder_Record implements UNL_Peoplefinder_Routable, Serializable
             }
         }
 
-        // inject methods as properties
-        $data['imageURL'] = $this->getImageURL();
+        if ($this->uid) {
+            // inject methods as properties
+            $data['imageURL'] = $this->getImageURL();
 
-        if ($address = $this->formatPostalAddress()) {
-            $data['unlDirectoryAddress'] = $address;
-        }
+            if ($address = $this->formatPostalAddress()) {
+                $data['unlDirectoryAddress'] = $address;
+            }
 
-        if ($this->shouldShowKnowledge()) {
-            $knowledge = $this->getKnowledge();
-            $data['knowledge'] = $knowledge->jsonSerialize();
+            if ($this->shouldShowKnowledge()) {
+                $knowledge = $this->getKnowledge();
+                $data['knowledge'] = $knowledge->jsonSerialize();
+            }
         }
 
         // for backwards compatibliity (safe), cast to object
@@ -750,6 +752,10 @@ class UNL_Peoplefinder_Record implements UNL_Peoplefinder_Routable, Serializable
     public function jsonSerialize()
     {
         $data = $this->getPublicProperties();
+
+        if (!$this->uid) {
+            return $data;
+        }
 
         //force uid to be a single value
         $data['uid'] = (string) $this->uid;
