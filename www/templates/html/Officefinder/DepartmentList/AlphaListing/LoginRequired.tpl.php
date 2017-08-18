617d0e5b5713434bdf5939ddc0a6404bc1f3d549
Eric Rasmussen <ericrasmussen1@gmail.com>
2017-08-18 15:44:28 -0500

    Format the paper printout better
    smaller text and hide full phone number if on-campus number can be printed

--- www/templates/html/Officefinder/DepartmentList/AlphaListing/LoginRequired.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/AlphaListing/LoginRequired.tpl.php
@@ -1,2 +1,32 @@
-<h2>Printer friendly version for <?php echo $context->user; ?>, on <?php echo date('F jS, Y'); ?></h2>
+<!doctype html>
+<html>
+<head>
+    <title>UNL Directory Prepared for <?php echo $context->user; ?> on <?php echo date('F jS, Y'); ?></title>
+    <style>
+        html {
+            font-size: 12px;
+            color: #000;
+        }
+        ol {
+            list-style: none;
+            padding-left: 1em;
+        }
+        a {
+            text-decoration: none;
+            color: #000;
+        }
+        abbr {
+            text-decoration: none;
+        }
+
+        .on-campus-dialing + a {
+            display: none;
+        }
+        .on-campus-label {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+<h2>University of Nebraska–Lincoln Directory Prepared for <?php echo $context->user; ?> on <?php echo date('F jS, Y'); ?></h2>
 <?php echo $savvy->render($context->output); ?>
