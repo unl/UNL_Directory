18f4ef3d81297b844e424646bf0ef7baffaebbc0
Eric Rasmussen <ericrasmussen1@gmail.com>
2017-08-18 15:51:57 -0500

    Add CSS columns to paper print document

--- www/templates/html/Officefinder/DepartmentList/AlphaListing/LoginRequired.tpl.php
+++ www/templates/html/Officefinder/DepartmentList/AlphaListing/LoginRequired.tpl.php
@@ -7,6 +7,11 @@
             font-size: 12px;
             color: #000;
         }
+        body {
+            column-count: 2;
+            -webkit-column-count: 2;
+            -moz-column-count: 2;
+        }
         ol {
             list-style: none;
             padding-left: 1em;
@@ -28,5 +33,5 @@
     </style>
 </head>
 <body>
-<h2>University of Nebraska–Lincoln Directory Prepared for <?php echo $context->user; ?> on <?php echo date('F jS, Y'); ?></h2>
+<h2>University of Nebraska–Lincoln Directory (Prepared for <?php echo $context->user; ?> on <?php echo date('F jS, Y'); ?>)</h2>
 <?php echo $savvy->render($context->output); ?>
