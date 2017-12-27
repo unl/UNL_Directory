9560fc96858173a5678748dc04189ccb19097d7c
Eric Rasmussen <ericrasmussen1@gmail.com>
2017-12-26 20:04:26 -0600

    Add <meta name="robots" content="noarchive">

--- www/templates/html/static/head.tpl.php
+++ www/templates/html/static/head.tpl.php
@@ -9,6 +9,7 @@ if (strpos($loginService, '//') === 0) {
 $loginUrl = 'https://login.unl.edu/cas/login?service=' . urlencode($loginService);
 $logoutUrl = 'https://login.unl.edu/cas/logout?url=' . urlencode($loginService);
 ?>
+<meta name="robots" content="noarchive">
 <meta name="description" content="The Faculty, Staff and Student online directory for the University. Information obtained from this directory may not be used to provide addresses for mailings to students, faculty or staff. Any solicitation of business, information, contributions or other response from individuals listed in this publication by mail, telephone or other means is forbidden."/>
 <meta name="keywords" content="university of nebraska-lincoln student faculty staff directory vcard"/>
 <meta name="author" content="University of Nebraska–Lincoln Office of University Communication"/>
