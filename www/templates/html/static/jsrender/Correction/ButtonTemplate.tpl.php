16196d249f83765d6e590f50fa5df6a605aa7231
Ryan Dee <ryan@skoolbus39.com>
2019-03-14 15:02:49 -0500

    Update print CSS
    - Add comments
    - Undo nesting
    - Combine rulesets
    - Remove `#visitorChat` and `.vcard-tools` from `display: none` ruleset (`#visitorChat` already using `.dcf-d-none@print` utility class)
    - Add `.dcf-d-none@print` utility class to `.vcard-tools`
    - Update `#maincontent` to `#dcf-main`
    - Remove `border: 0` for links (Borders are no longer used for styling links)
    - Add `!important` to link `text-decoration`
    - Remove photo sizing (already sized by utility classes)

--- www/templates/html/static/jsrender/Correction/ButtonTemplate.tpl.php
+++ www/templates/html/static/jsrender/Correction/ButtonTemplate.tpl.php
@@ -1,4 +1,4 @@
-<div class="vcard-tools wdn_vcardTools dcf-d-flex dcf-jc-center dcf-txt-sm dcf-pt-3 dcf-pb-3 dcf-bt-1 dcf-bt-solid unl-bt-light-gray">
+<div class="vcard-tools wdn_vcardTools dcf-d-flex dcf-jc-center dcf-txt-sm dcf-pt-3 dcf-pb-3 dcf-bt-1 dcf-bt-solid unl-bt-light-gray dcf-d-none@print">
     <button class="dcf-btn dcf-btn-secondary dcf-b-0 correction edit-button">
         <span class="icon-pencil" aria-hidden="true"></span>
         Suggest a correction <span class="dcf-sr-only">for {{:preferredName}}</span>
