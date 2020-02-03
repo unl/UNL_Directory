3c9b766e50ad447599aaada36f197a572420d883
Ryan Dee <ryan@skoolbus39.com>
2020-02-03 15:31:44 -0600

    Update indentation to four spaces

--- www/templates/html/static/contact-info.tpl.php
+++ www/templates/html/static/contact-info.tpl.php
@@ -1,17 +1,17 @@
 <nav id="dcf-footer-group-1" role="navigation" aria-labelledby="dcf-footer-group-1-heading">
-  <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About the Directory</h3>
-  <p><a href="<?php echo UNL_Peoplefinder::getURL() . 'help/' ?>"><span class="wdn-icon-info" aria-hidden="true"></span>Directory Help</a></p>
-  <p><a href="http://wdn.unl.edu/documentation/unl-directory"z<span class="wdn-icon-rocket" aria-hidden="true"></span>Developer Documentation</a></p>
-  <p>Information obtained from this directory may not be used to provide addresses for mailings to students, faculty or staff. Any solicitation of business, information, contributions or other response from individuals listed in this publication by mail, telephone or other means is forbidden.</p>
-  <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group at Nebraska</a>. DXG is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
-  <p>This website is protected against malicious content robots and abusive web browsers. If you experience an issue or receive an error/blocked message while browsing, please immediately report the problem to the <a href="http://its.unl.edu/helpcenter">ITS Help Center</a>.</p>
+    <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About the Directory</h3>
+    <p><a href="<?php echo UNL_Peoplefinder::getURL() . 'help/' ?>"><span class="wdn-icon-info" aria-hidden="true"></span>Directory Help</a></p>
+    <p><a href="http://wdn.unl.edu/documentation/unl-directory"><span class="wdn-icon-rocket" aria-hidden="true"></span>Developer Documentation</a></p>
+    <p>Information obtained from this directory may not be used to provide addresses for mailings to students, faculty or staff. Any solicitation of business, information, contributions or other response from individuals listed in this publication by mail, telephone or other means is forbidden.</p>
+    <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group at Nebraska</a>. DXG is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
+    <p>This website is protected against malicious content robots and abusive web browsers. If you experience an issue or receive an error/blocked message while browsing, please immediately report the problem to the <a href="http://its.unl.edu/helpcenter">ITS Help Center</a>.</p>
 </nav>
 <nav id="dcf-footer-group-2" role="navigation" aria-labelledby="dcf-footer-group-2-heading">
-  <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-2-heading">Related Links</h3>
-  <ul class="dcf-list-bare dcf-mb-0">
-    <li><a href="https://wdn.unl.edu/">Web Developer Network</a></li>
-    <li><a href="https://dxg.unl.edu/">Digital Experience Group</a></li>
-    <li><a href="https://ucomm.unl.edu/">University Communication</a></li>
-    <li><a href="https://its.unl.edu/">Information Technology Services</a></li>
-  </ul>
+    <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-2-heading">Related Links</h3>
+    <ul class="dcf-list-bare dcf-mb-0">
+        <li><a href="https://wdn.unl.edu/">Web Developer Network</a></li>
+        <li><a href="https://dxg.unl.edu/">Digital Experience Group</a></li>
+        <li><a href="https://ucomm.unl.edu/">University Communication</a></li>
+        <li><a href="https://its.unl.edu/">Information Technology Services</a></li>
+    </ul>
 </nav>
