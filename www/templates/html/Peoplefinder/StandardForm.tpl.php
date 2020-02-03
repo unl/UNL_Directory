8fceb7f76109f29a787f602c4166267c91587e90
Ryan Dee <ryan@skoolbus39.com>
2020-02-03 15:28:52 -0600

    Replace search submit button icon with SVG

--- www/templates/html/Peoplefinder/StandardForm.tpl.php
+++ www/templates/html/Peoplefinder/StandardForm.tpl.php
@@ -11,7 +11,12 @@
     ?>
     <div class="dcf-input-group dcf-measure">
         <input class="dcf-input-text" tabindex="0" type="text" autofocus placeholder="Herbie Husker" value="<?php echo $default; ?>" id="q" name="q" aria-label="Enter a name to begin your search" />
-        <button name="submitbutton" type="submit" value="Search" class="button dcf-btn dcf-btn-primary"><span class="wdn-icon-search" aria-hidden="true"></span><span class="dcf-sr-only">Search</span></button>
+        <button name="submitbutton" type="submit" value="Search" class="button dcf-btn dcf-btn-primary">
+            <svg class="dcf-h-5 dcf-w-5 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48">
+                <path d="M18 36a17.9 17.9 0 0 0 11.27-4l15.31 15.41a2 2 0 0 0 2.84-2.82L32.08 29.18A18 18 0 1 0 18 36zm0-32A14 14 0 1 1 4 18 14 14 0 0 1 18 4z"></path>
+            </svg>        
+            <span class="dcf-sr-only">Search</span>
+        </button>
     </div>
 </form>
 
