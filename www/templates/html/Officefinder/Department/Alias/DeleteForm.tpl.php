4e8f4f3ffffa2bc3c7edf63f63de52b5065aaf8f
Ryan Dee <ryan@skoolbus39.com>
2020-02-04 13:40:15 -0600

    Replace trash icons with SVGs

--- www/templates/html/Officefinder/Department/Alias/DeleteForm.tpl.php
+++ www/templates/html/Officefinder/Department/Alias/DeleteForm.tpl.php
@@ -4,5 +4,11 @@
     <input type="hidden" name="name" value="<?php echo $context->name; ?>" />
     <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenNameKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenName() ?>" />
     <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenValueKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenValue() ?>">
-    <button class="dcf-btn dcf-btn-inverse-secondary" type="submit"><span class="icon-trash" aria-hidden="true"></span><span class="dcf-sr-only">Delete</span></button>
+    <button class="dcf-btn dcf-btn-inverse-secondary" type="submit">
+        <svg class="dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="24" width="24" viewBox="0 0 24 24">
+            <path d="M23 3h-7V.5a.5.5 0 00-.5-.5h-8a.5.5 0 00-.5.5V3H1a.5.5 0 000 1h2v19.5a.5.5 0 00.5.5h16a.5.5 0 00.5-.5V4h3a.5.5 0 000-1zM8 1h7v2H8V1zm11 22H4V4h15v19z"></path>
+            <path d="M7.5 6.5A.5.5 0 007 7v12a.5.5 0 001 0V7a.5.5 0 00-.5-.5zm4 0a.5.5 0 00-.5.5v12a.5.5 0 001 0V7a.5.5 0 00-.5-.5zM15 7v12a.5.5 0 001 0V7a.5.5 0 00-1 0z"></path>
+        </svg>
+        <span class="dcf-sr-only">Delete</span>
+    </button>
 </form>
