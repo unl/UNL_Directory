ca18f2374928f0ad830f58c65c24b849be1e8972
Ryan Dee <ryan@skoolbus39.com>
2020-02-04 16:52:25 -0600

    Replace trash icon with SVG

--- www/templates/html/Officefinder/Department/Listing.tpl.php
+++ www/templates/html/Officefinder/Department/Listing.tpl.php
@@ -49,7 +49,13 @@ if (!empty($context->email)) {
                 <div class="form"></div>
                 <div class="add-form"></div>
                 <?php echo $savvy->render($context, 'Officefinder/Department/DeleteForm.tpl.php') ?>
-                <button type="submit" class="dcf-btn dcf-btn-primary" form="deletedepartment_<?php echo $context->id ?>"><span class="icon-trash" aria-hidden="true"></span>Delete</button>
+                <button type="submit" class="dcf-btn dcf-btn-primary" form="deletedepartment_<?php echo $context->id ?>">
+                    <svg class="dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="24" width="24" viewBox="0 0 24 24">
+                        <path d="M23 3h-7V.5a.5.5 0 00-.5-.5h-8a.5.5 0 00-.5.5V3H1a.5.5 0 000 1h2v19.5a.5.5 0 00.5.5h16a.5.5 0 00.5-.5V4h3a.5.5 0 000-1zM8 1h7v2H8V1zm11 22H4V4h15v19z"></path>
+                        <path d="M7.5 6.5A.5.5 0 007 7v12a.5.5 0 001 0V7a.5.5 0 00-.5-.5zm4 0a.5.5 0 00-.5.5v12a.5.5 0 001 0V7a.5.5 0 00-.5-.5zM15 7v12a.5.5 0 001 0V7a.5.5 0 00-1 0z"></path>
+                    </svg>
+                    Delete
+                </button>
             </div>
         </div>
     <?php endif; ?>
