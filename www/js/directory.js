3b5df8609ed51a321c9635d9421e4dc4e49ea443
Jeff Sturek <jsturek8@unl.edu>
2020-04-24 15:08:00 -0500

    Edit button hide and show with ready

--- www/js/directory.js
+++ www/js/directory.js
@@ -801,11 +801,11 @@ define([
 
 			return false;
 		});
-
-		// show hidden edit buttons (hidden with dcf-d-none)
-		$(".edit-button").removeClass("dcf-d-none");
 	};
 
+	// show hidden edit buttons (hidden with dcf-d-none)
+	$(".edit-button").removeClass("dcf-d-none");
+	
 	var ajaxSubmitToDepartmentList = function(form, context, listClass, tmpl, data) {
 		$.post(form.action + '?' + $.param({redirect: '0'}), $(form).serialize(), function() {
 			var $newItem = $(tmpl.render(data)).hide();
