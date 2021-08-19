0c8dc11852fd4c8e4fb82fcdbf06e11318d7181e
Jeff Sturek <jsturek@gmail.com>
2021-08-19 09:05:53 -0500

    Final Draft

--- www/templates/html/Peoplefinder/Help.tpl.php
+++ www/templates/html/Peoplefinder/Help.tpl.php
@@ -7,12 +7,12 @@ if (isset($page)) {
     $page->doctitle = substr_replace($page->doctitle, 'Help | ', strlen('<title>'), 0);
 }
 ?>
-<section class="dcf-bleed unl-bg-lightest-gray dcf-pt-6  dcf-pb-6 dcf-txt-sm" id="data">
+<section class="dcf-bleed unl-bg-lightest-gray dcf-pt-6  dcf-pb-6" id="data">
     <div class="dcf-wrapper">
         <h2 class="dcf-txt-center clear-top">Finding Your Way Around the Directory</h2>
-        <div class="dcf-grid-thirds@md dcf-col-gap-vw dcf-row-gap-3 dcf-pt-3">
-            <div>
-                <h4>About the data</h4>
+        <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-3 dcf-pt-3">
+            <div class="dcf-col-100% dcf-col-67%-start@md">
+                <h3 class="dcf-txt-h5">About the data</h3>
                 <p>
                     The data used to build the <a href="https://directory.unl.edu/">Directory</a> is maintained in several source databases.
                     Information obtained from the directory may <em>not</em> be used to provide addresses for mailings to students, faculty or staff.
@@ -22,44 +22,20 @@ if (isset($page)) {
                     <strong><em>Student</em></strong> directory data is maintained in the university student information system.
                 </p>
                 <p>
-                    <strong><em>Faculty/Staff</em></strong> directory data is maintained in the university administrative and finance system, SAP.
-                    The SAP data is the official, business-oriented information about each person employed by the university. This data includes:
-                </p>
-                <ul>
-                    <li>name</li>
-                    <li>employment designation</li>
-                    <li>job title(s)</li>
-                    <li>department(s)</li>
-                    <li>campus address</li>
-                    <li>campus phone number</li>
-                    <li>campus email address</li>
-                </ul>
+                    <strong><em>Faculty/Staff</em></strong> directory data is maintained in the university administrative and finance system, SAP / Firefly.</p>
                 <p>
-                    <strong><em>Department/Listing</em></strong> directory data is sourced from the university administrative and finance system, SAP, and augmented with information provided by departmental HR coordinators.
+                    The <strong><em>Departments &amp; Units</em></strong> directory is primarily composed of information provided by departmental HR coordinators, and is augmented with data sourced from the university administrative and finance system.
                 </p>
-            </div>
-            <div>
-                <h4>Updating your information</h4>
-
+                <h3 class="dcf-txt-h5">Updating Your Information</h3>
                 <p><strong>Personal information</strong> is managed outside of the directory. See our <a href="https://www.unl.edu/how-manage-personal-information/">How to Manage Personal Information</a> page for options to update your information.</p>
-
-                <p>Directory specific personal information details:</p>
-                <ul>
-                    <li>As a <strong><em>student</em></strong>, you may request that your information not be listed in the directory by contacting the <a href="https://registrar.unl.edu/">Office of the University Registrar</a> to set a privacy flag on your account.
-                        If any information is displayed incorrectly, please consult your academic advisor.</li>
-                    <li>As a <strong><em>faculty/staff</em></strong> member, you may update your preferred <code>&commat;unl&period;edu</code> address by visiting the <a href="https://its.unl.edu/emailhome/activation">email activation site</a>.
-                        If you are also registered as a student, you will need to contact the <a href="https://registrar.unl.edu/">Office of the University Registrar</a> to set a privacy flag on your account to remove your student information from the directory.
-                        For all other data changes, contact your HR representative or departmental SAP support staff.</li>
-                    <li>As a <strong><em>retiree</em></strong>, if you have no current HR representative, you should contact the <a href="https://hr.unl.edu/benefits/">Benefits Office</a>.</li>
-                </ul>
                 <p>
-                    <strong>Department listings</strong> are managed within the directory. Directory information update requests may be emailed to <a href="mailto:operator&commat;unl&period;edu">operator&commat;unl&period;edu</a> with the directory URL of the listing and the information you'd like to update.
+                    <b>Department / Unit listings</b> in directory have an HR coordinator who is responsible for their respective listing page. The "Suggest a correction" button on each department / unit listing page will submit a request to the editors for that respective listing.
                 </p>
             </div>
-            <div>
-                <h4 id="photo">Adding your photo</h4>
+            <div class="dcf-col-100% dcf-col-33%-end@md">
+                <h3 class="dcf-txt-h5" id="photo">Adding your photo</h3>
                 <div>
-                    <img class="dcf-obj-fit-cover dcf-mb-3" src="<?php echo $baseUrl ?>images/130912herbie-505.jpg" alt="Profile view of mascot Herbie Husker" />
+                    <img class="dcf-obj-fit-cover dcf-mb-3" src="<?php echo $baseUrl ?>images/130912herbie-505.jpg" alt="Profile view of mascot Herbie Husker">
                 </div>
                 <p>
                     Want to show your photo on your Directory result instead of the default outline? We've got a couple of ways to do that.
@@ -68,7 +44,6 @@ if (isset($page)) {
                 </p>
                 <p>As a member of the university community, we've got a <a href="https://planetred.unl.edu/">Planet Red</a> profile waiting for you to upload a photo to. All you need to do is fill out a little profile information and we'll automatically pull your photo from there.</p>
                 <p>If you'd prefer not to create a Planet Red profile and you are a faculty or staff member that has been issued a campus email address, you can use that address to create a <a href="https://en.gravatar.com/">Gravatar</a> profile. Once you've got your profile loaded with an avatar, we'll automatically pull your photo from there.</p>
-                <p>Department listings may use the email address entered in the directory with the Gravatar service. If an email address is not provided or a Gravatar profile is not found for it, a photo of the building the listing is in will be used.</p>
             </div>
         </div>
     </div>
