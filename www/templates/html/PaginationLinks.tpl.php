6f86aed8acd1861d96b14c6c33aa5fca516fb83c
Jeff Sturek <jsturek@gmail.com>
2021-12-03 12:36:29 -0600

    Clean up code smell

--- www/templates/html/PaginationLinks.tpl.php
+++ www/templates/html/PaginationLinks.tpl.php
@@ -1,10 +1,14 @@
+<?php
+    const AMP_LIMIT = '&amp;limit=';
+    const AMP_OFFSET = '&amp;offset=';
+?>
 <nav class="dcf-pagination">
     <ol class="dcf-list-bare dcf-list-inline">
         <?php if ($context->offset != 0) :?>
-        <li><a class="dcf-pagination-prev" href="<?php echo $context->url.'&amp;limit='.$context->limit.'&amp;offset='.($context->offset-$context->limit); ?>">Prev</a></li>
+        <li><a class="dcf-pagination-prev" href="<?php echo $context->url.AMP_LIMIT.$context->limit.AMP_OFFSET.($context->offset-$context->limit); ?>">Prev</a></li>
         <?php endif; ?>
         <?php for ($page = 1; $page*$context->limit < $context->total+$context->limit; $page++ ) {
-            $link = $context->url.'&amp;limit='.$context->limit.'&amp;offset='.($page-1)*$context->limit;
+            $link = $context->url.AMP_LIMIT.$context->limit.AMP_OFFSET.($page-1)*$context->limit;
             $currentPage = false;
             if (($page-1)*$context->limit == $context->offset) {
 	            $currentPage = true;
@@ -19,7 +23,7 @@
         </li>
         <?php } // end for?>
         <?php if (($context->offset+$context->limit) < $context->total) :?>
-        <li><a class="dcf-pagination-next" href="<?php echo $context->url.'&amp;limit='.$context->limit.'&amp;offset='.($context->offset+$context->limit); ?>">Next</a></li>
+        <li><a class="dcf-pagination-next" href="<?php echo $context->url.AMP_LIMIT.$context->limit.AMP_OFFSET.($context->offset+$context->limit); ?>">Next</a></li>
         <?php endif; ?>
     </ol>
 </nav>
