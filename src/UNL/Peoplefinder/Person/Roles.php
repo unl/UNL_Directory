606bc3e21d7f552e966ddcf225e20b316974a363
Jeff Sturek <jsturek@gmail.com>
2022-01-11 13:05:10 -0600

    Update conditional

--- src/UNL/Peoplefinder/Person/Roles.php
+++ src/UNL/Peoplefinder/Person/Roles.php
@@ -38,12 +38,12 @@ class UNL_Peoplefinder_Person_Roles extends IteratorIterator implements Countabl
         return $this;
     }
 
-	public function isDisplayableRole($role) {
-		if ($role->unlRoleHROrgUnitNumber === UNL_Peoplefinder::ORG_UNIT_NUMBER_RETIREE) {
-			return false;
-		}
-		return true;
-	}
+    public function isDisplayableRole($role) {
+        if ($role->unlRoleHROrgUnitNumber == UNL_Peoplefinder::ORG_UNIT_NUMBER_RETIREE) {
+            return false;
+        }
+        return true;
+    }
 
     /**
      * Get the number of roles this person has
