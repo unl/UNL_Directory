0f959e0dbf5357df39084a2b4e9e7614a6db492a
Thomas Neumann <thomasneumann21800@gmail.com>
2024-04-25 14:19:24 -0500

    Updated footer links

--- www/templates/html/static/contact-info.tpl.php
+++ www/templates/html/static/contact-info.tpl.php
@@ -1,8 +1,80 @@
 <div id="dcf-footer-group-1">
     <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About the Directory</h3>
     <ul class="dcf-mb-4" aria-labelledby="dcf-footer-group-1-heading" role="list">
-        <li><a href="<?php echo UNL_Peoplefinder::getURL() . 'help/' ?>"><svg class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 24 24"><path d="M11.5 1.004C5.158 1.004 0 6.162 0 12.504s5.158 11.5 11.5 11.5c6.341 0 11.5-5.158 11.5-11.5s-5.159-11.5-11.5-11.5zm-.5 5a1.001 1.001 0 11-.002 2.002A1.001 1.001 0 0111 6.004zm3.5 14h-6a.5.5 0 010-1h2.499v-8H9.5a.5.5 0 010-1h2a.5.5 0 01.499.5v8.5H14.5a.5.5 0 010 1z"></path></svg>Directory Help</a></li>
-        <li><a href="https://wdn.unl.edu/documentation/unl-directory"><svg class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 24 24"><path d="M15 0H9v20.707l3-3 3 3zM8 0H5.5a.5.5 0 00-.5.5v23a.5.5 0 00.853.354L8 21.707V0zm8 0h2.5a.5.5 0 01.5.5v23a.501.501 0 01-.854.354L16 21.707V0z"></path></svg>Developer Documentation</a></li>
+        <li>
+            <a href="<?php echo UNL_Peoplefinder::getURL() . 'myinfo' ?>">
+                <svg
+                    class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-fill-current"
+                    aria-hidden="true"
+                    focusable="false"
+                    height="16"
+                    width="16"
+                    viewBox="0 0 48 48"
+                >
+                    <path d="M47.9 24C47.9 10.8 37.2.1 24 .1S.1 10.8.1 24
+                        c0 6.3 2.5 12.3 6.9 16.8 4.5 4.6 10.6 7.1 17 7.1
+                        s12.5-2.5 17-7.1c4.5-4.5 6.9-10.5 6.9-16.8zm-45
+                        0C2.9 12.4 12.4 2.9 24 2.9c11.6 0 21.1 9.5 21.1
+                        21.1 0 5.2-1.9 10.1-5.3 14-2.1-1.2-5-2.2-8.2-3.4-.7-.3-1.5-.5-2.2-.8
+                        v-3.1c1.1-.7 2.6-2.4 2.9-5.7.8-.6 1.2-1.6 1.2-2.9
+                        0-1.1-.4-2.1-1-2.7.5-1.6 1.3-4.2.7-6.5-.7-3-4.6-4-7.7-4-2.7
+                        0-5.9.8-7.2 2.8-1.2 0-2 .5-2.4 1-1.6 1.7-.8 4.8-.3 6.6-.6.6-1
+                        1.6-1 2.7 0 1.3.5 2.3 1.2 2.9.3 3.4 1.8 5 2.9 5.7
+                        v3.1c-.7.2-1.4.5-2 .7-3.1 1.1-6.2 2.2-8.4
+                        3.5-3.5-3.7-5.4-8.7-5.4-13.9zm7.5 16.1c2-1 4.6-2
+                        7.2-2.9 1-.4 2-.7 3-1.1.5-.2.9-.7.9-1.3v-4.9
+                        c0-.6-.4-1.1-.9-1.3-.1 0-2-.8-2-4.5
+                        0-.7-.5-1.2-1.1-1.4-.1-.3-.1-.9 0-1.2.6-.1 1.1-.7
+                        1.1-1.4 0-.3-.1-.6-.2-1.2-.9-3.2-.7-4-.4-4.3.1-.1.4-.1
+                        1 0 .7.1 1.5-.3 1.6-1 .3-1 2.5-1.9 5-1.9s4.7.8 5
+                        1.9c.4 1.7-.4 4.1-.7 5.2-.2.6-.3.9-.3 1.3 0
+                        .7.5 1.2 1.1 1.4.1.3.1.9 0 1.2-.6.1-1.1.7-1.1 1.4
+                        0 3.7-1.9 4.5-2 4.5-.6.2-1 .7-1 1.3v4.9c0 .6.4
+                        1.1.9 1.3 1.1.4 2.1.8 3.2 1.2 2.7 1 5.2 1.9 7.1 2.8-3.8
+                        3.3-8.6 5-13.7 5-5.2 0-9.9-1.8-13.7-5z"
+                    ></path>
+                </svg>
+                Update My Avatar
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo UNL_Peoplefinder::getURL() . 'help/' ?>">
+                <svg
+                    class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-fill-current"
+                    aria-hidden="true"
+                    focusable="false"
+                    height="16"
+                    width="16"
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M11.5 1.004C5.158 1.004 0 6.162 0 12.504s5.158 11.5 11.5 11.5
+                        c6.341 0 11.5-5.158 11.5-11.5s-5.159-11.5-11.5-11.5zm-.5 5a1.001 1.001 0 11-.002 2.002
+                        A1.001 1.001 0 0111 6.004zm3.5 14h-6a.5.5 0 010-1h2.499v-8
+                        H9.5a.5.5 0 010-1h2a.5.5 0 01.499.5v8.5H14.5a.5.5 0 010 1z"
+                    ></path>
+                </svg>
+                Directory Help
+            </a>
+        </li>
+        <li>
+            <a href="https://wdn.unl.edu/documentation/unl-directory">
+                <svg
+                    class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-fill-current"
+                    aria-hidden="true"
+                    focusable="false"
+                    height="16"
+                    width="16"
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M15 0H9v20.707l3-3 3 3zM8 0H5.5a.5.5 0
+                        00-.5.5v23a.5.5 0 00.853.354L8 21.707V0zm8
+                        0h2.5a.5.5 0 01.5.5v23a.501.501 0 01-.854.354L16
+                        21.707V0z"
+                    ></path>
+                </svg>
+                Developer Documentation
+            </a>
+        </li>
     </ul>
     <p>Information obtained from this directory may not be used to provide addresses for mailings to students, faculty or staff. Any solicitation of business, information, contributions or other response from individuals listed in this publication by mail, telephone or other means is forbidden.</p>
     <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group</a> at <a href="https://www.unl.edu/">Nebraska</a>. DXG is a part of <a href="https://ucomm.unl.edu/">University Communication &amp; Marketing</a>.</p>
