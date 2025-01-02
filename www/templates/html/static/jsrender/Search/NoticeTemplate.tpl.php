b1b9b2bdb3e9eae367127e4fa77095711eb7f418
Thomas Neumann <thomasneumann21800@gmail.com>
2025-01-02 08:16:09 -0600

    Escape html in jsrender

--- www/templates/html/static/jsrender/Search/NoticeTemplate.tpl.php
+++ www/templates/html/static/jsrender/Search/NoticeTemplate.tpl.php
@@ -1,8 +1,8 @@
 <div class="dcf-notice dcf-notice-info" hidden>
     <h2>We automatically tried some other searches for you.</h2>
     <div>
-        <p>Your original search for <span>{{:originalSearch}}</span> did not return any results.
-        So we tried a few more advanced searches and below is what we found for <span>First Name: {{:firstName}} AND Last Name: {{:lastName}}</span>.</p>
+        <p>Your original search for <span>{{>:originalSearch}}</span> did not return any results.
+        So we tried a few more advanced searches and below is what we found for <span>First Name: {{>:firstName}} AND Last Name: {{>:lastName}}</span>.</p>
     </div>
 </div>
 {{*
--- www/templates/html/static/jsrender/SearchResults/OptionTemplate.tpl.php
+++ www/templates/html/static/jsrender/SearchResults/OptionTemplate.tpl.php
@@ -1,4 +1,4 @@
 <li class="dcf-input-checkbox">
     <input id="filter{{:type}}" value="{{:type}}" type="checkbox"/>
-    <label class="dcf-label" for="filter{{:type}}">{{:label}}</label>
+    <label class="dcf-label" for="filter{{:type}}">{{>:label}}</label>
 </li>
