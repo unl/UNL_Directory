59c7ae335ecdbb34e77dca78085ecff9fa0757dc
Thomas Neumann <thomasneumann21800@gmail.com>
2025-01-06 15:37:01 -0600

    Reverted jsrender changes

--- www/templates/html/static/jsrender/SearchResults/OptionListTemplate.tpl.php
+++ www/templates/html/static/jsrender/SearchResults/OptionListTemplate.tpl.php
@@ -1,6 +1,6 @@
 <ol>
     <li class="dcf-input-checkbox">
         <input id="filterAll{{:type}}" class="filterAll" type="checkbox" checked/>
-        <label class="dcf-label" for="filterAll{{:type}}">All<span class="dcf-sr-only"> {{>:type}}</label>
+        <label class="dcf-label" for="filterAll{{:type}}">All<span class="dcf-sr-only"> {{:type}}</label>
     </li>
 </ol>
