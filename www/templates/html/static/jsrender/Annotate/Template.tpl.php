5af8a0e1b9af199b39f3ea389628cf6b0854c595
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2025-01-08 15:26:56 -0600

    hide annotate since the shib policies prevent embed (#232)

--- www/templates/html/static/jsrender/Annotate/Template.tpl.php
+++ www/templates/html/static/jsrender/Annotate/Template.tpl.php
@@ -1,4 +1,4 @@
-<a class="directory_annotate dcf-btn dcf-btn-secondary dcf-b-0 dcf-d-flex dcf-flex-col dcf-ai-center" href="{{>annotateUrl}}" title="Leave notes on the listing for {{>preferredName}}">
+<a style="display: none !important;" class="directory_annotate dcf-btn dcf-btn-secondary dcf-b-0 dcf-d-flex dcf-flex-col dcf-ai-center" href="{{>annotateUrl}}" title="Leave notes on the listing for {{>preferredName}}">
     <svg class="dcf-mb-2 dcf-h-7 dcf-w-7 dcf-fill-current" aria-hidden="true" focusable="false" height="24" width="24" viewBox="0 0 24 24">
         <path d="M23 16.5v-13C23 2.122 21.878 1 20.5 1h-17A2.503 2.503 0 001 3.5v16C1 20.878 2.122 22 3.5 22H18c.142 0 .277-.06.372-.166l4.5-5c.022-.024.034-.054.05-.082.014-.022.031-.042.041-.067A.504.504 0 0023 16.5zm-21 3v-16C2 2.673 2.673 2 3.5 2h17c.827 0 1.5.673 1.5 1.5V16h-4.5a.5.5 0 00-.5.5V21H3.5c-.827 0-1.5-.673-1.5-1.5zm16 1.252V17h3.377L18 20.752z"></path>
         <path d="M15 16H8v-4h12a.5.5 0 000-1H8V7h12a.5.5 0 000-1H8V4a.5.5 0 00-1 0v2H4a.5.5 0 000 1h3v4H4a.5.5 0 000 1h3v4H4a.5.5 0 000 1h3v2a.5.5 0 001 0v-2h7a.5.5 0 000-1z"></path>
