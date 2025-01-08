c5dfc17eb8a2bd8545d99dfce46d20cff5e020dc
Thomas Neumann <thomasneumann21800@gmail.com>
2025-01-08 15:15:01 -0600

    Escape things correctly

--- www/templates/html/static/jsrender/Department/AliasTemplate.tpl.php
+++ www/templates/html/static/jsrender/Department/AliasTemplate.tpl.php
@@ -1,9 +1,9 @@
 <li>
-    <span>{{:name}}</span>
-    <form action="{{:url}}" method="post" class="delete dcf-form">
+    <span>{{>name}}</span>
+    <form action="{{>url}}" method="post" class="delete dcf-form">
         <input type="hidden" name="_type" value="delete_dept_alias" />
-        <input type="hidden" name="department_id" value="{{:department}}" />
-        <input type="hidden" name="name" value="{{:name}}" />
+        <input type="hidden" name="department_id" value="{{>department}}" />
+        <input type="hidden" name="name" value="{{>name}}" />
         <button class="dir-btn-delete dcf-btn dcf-btn-primary" type="submit">
             <svg class="dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="24" width="24" viewBox="0 0 24 24">
                 <path d="M23 3h-7V.5a.5.5 0 00-.5-.5h-8a.5.5 0 00-.5.5V3H1a.5.5 0 000 1h2v19.5a.5.5 0 00.5.5h16a.5.5 0 00.5-.5V4h3a.5.5 0 000-1zM8 1h7v2H8V1zm11 22H4V4h15v19z"></path>
--- www/templates/html/static/jsrender/SearchResults/SummaryFilterTemplate.tpl.php
+++ www/templates/html/static/jsrender/SearchResults/SummaryFilterTemplate.tpl.php
@@ -1,1 +1,1 @@
-<span class="{{:filterValue}} selected-options"><span class="group">{{:filterType}}</span> {{:filterLabel}}</span> <span class="operator">OR </span>
+<span class="{{>filterValue}} selected-options"><span class="group">{{>filterType}}</span> {{>filterLabel}}</span> <span class="operator">OR </span>
