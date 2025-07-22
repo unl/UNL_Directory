c4512c4599b9b098b8733a7a42ca0c95f71da313
Thomas Neumann <thomasneumann21800@gmail.com>
2025-07-22 09:09:30 -0500

    Add new appointments field

--- www/templates/json/Peoplefinder/Record.tpl.php
+++ www/templates/json/Peoplefinder/Record.tpl.php
@@ -10,6 +10,24 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
 
+/**
+ * This comes from UNL_Peoplefinder_Person_Roles
+ */
+function isDisplayableRole($role) {
+    if ($role->unlRoleHROrgUnitNumber == UNL_Peoplefinder::ORG_UNIT_NUMBER_RETIREE ||
+        strtolower($role->description) == 'retiree') {
+        return false;
+    }
+    return true;
+}
+
+// Get all the roles
+$roles = $context->getRoles();
+foreach ($roles as $single_role) {
+    if (!isDisplayableRole($single_role)) { continue; }
+    $context->appointments[] = $single_role;
+}
+
 if (empty($ip) || !in_array($ip, UNL_Peoplefinder::$allowed_unluncwid_IPs)) {
     unset($context->unluncwid);
 }
